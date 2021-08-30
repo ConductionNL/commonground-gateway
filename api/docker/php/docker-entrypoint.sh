88e23f30b1cb310c97f7e58a7e321d4bff65b3c5
ginokok1996 <ginokok1996@hotmail.com>
2021-08-30 11:25:35 +0200

    line endings

--- api/docker/php/docker-entrypoint.sh
+++ api/docker/php/docker-entrypoint.sh
@@ -17,7 +17,7 @@ if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 	setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX var
 	setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX var
 
-	composer install --prefer-dist --no-progress --no-interaction
+	composer install --prefer-dist --no-progress --no-suggest --no-interaction
 
 	mkdir -p /cert
 
