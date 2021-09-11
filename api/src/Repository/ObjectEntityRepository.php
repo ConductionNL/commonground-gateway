b99b88ce997fe4d366e24bed71d096845e05cab7
Ruben van der Linde <ruben@conduction.nl>
2021-09-11 15:57:07 +0200

    Added repositories and datafixture for regsitration

---
+++ api/src/Repository/ObjectEntityRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\ObjectEntity;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method ObjectEntity|null find($id, $lockMode = null, $lockVersion = null)
+ * @method ObjectEntity|null findOneBy(array $criteria, array $orderBy = null)
+ * @method ObjectEntity[]    findAll()
+ * @method ObjectEntity[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class ObjectEntityRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, ObjectEntity::class);
+    }
+
+    // /**
+    //  * @return ObjectEntity[] Returns an array of ObjectEntity objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('o')
+            ->andWhere('o.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('o.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?ObjectEntity
+    {
+        return $this->createQueryBuilder('o')
+            ->andWhere('o.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
