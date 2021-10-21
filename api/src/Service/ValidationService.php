9cd95a8c2c6f1e5c2ad67f0b4ce138287358f293
Wilco Louwerse <WilcoLouwerse@users.noreply.github.com>
2021-10-21 14:36:08 +0000

    Apply fixes from StyleCI

--- api/src/Service/ValidationService.php
+++ api/src/Service/ValidationService.php
@@ -268,7 +268,7 @@ class ValidationService
                 }
                 // If we are doing a PUT with a single subObject (and it contains no id) and the existing mainObject only has a single subObject, use the existing subObject and update that.
                 elseif (count($value) == 1 && count($valueObject->getObjects()) == 1) {
-                        $subObject = $valueObject->getObjects()->first();
+                    $subObject = $valueObject->getObjects()->first();
                 }
                 // Create a new subObject (ObjectEntity)
                 else {
