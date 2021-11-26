f8219a1e0c7d9fc4b805f0739003b7cc0d1bae43
Ruben van der Linde <ruben@conduction.nl>
2021-11-26 11:32:33 +0100

    Small Conviance on the soap API

--- api/src/Entity/Soap.php
+++ api/src/Entity/Soap.php
@@ -5,8 +5,11 @@ namespace App\Entity;
 use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\SoapRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
 
 /**
+ * This enity is used to transle incommong and outgoind soap calls
+ *
  * @ApiResource()
  * @ORM\Entity(repositoryClass=SoapRepository::class)
  */
@@ -20,35 +23,47 @@ class Soap
     private $id;
 
     /**
+     * The internal name of this soap call
+     *
      * @ORM\Column(type="string", length=255)
      */
     private $name;
 
     /**
+     * A short description of this soap call
+     *
      * @ORM\Column(type="text", nullable=true)
      */
     private $description;
 
     /**
+     * The message type of this soap translation
+     *
      * @ORM\Column(type="string", length=255)
      */
     private $type;
 
     /**
+     * The entity form the EAV stack that this SOAP connection wants to use
+     *
      * @ORM\ManyToOne(targetEntity=Entity::class, inversedBy="soap")
      * @ORM\JoinColumn(nullable=false)
      */
     private $entity;
 
     /**
+     * An XML descriping the request that we want to recieve
+     *
+     * @ORM\Column(type="text")
+     */
+    private $request;
+
+    /**
+     * An array build of request that we want to recieve
+     *
      * @ORM\Column(type="array")
      */
     private $requestSkeleton = [];
-
-    /**
-     * @ORM\Column(type="array")
-     */
-    private $responceSkeleton = [];
 
     /**
      * An array containing an request to entity translation in dot notation e.g. contact.firstname => person.name
@@ -58,11 +73,26 @@ class Soap
     private $requestHydration = [];
 
     /**
+     * An XML descriping the responce that we want t0 send
+     *
+     * @ORM\Column(type="text")
+     */
+    private $reponce;
+
+    /**
+     * An array build of responce that  we want to send
+     *
+     * @ORM\Column(type="array")
+     */
+    private $responceSkeleton = [];
+
+    /**
      * An array containing an entity to reponce transaltion in dot notation e.g. person.name => contact.firstname
      *
      * @ORM\Column(type="array", nullable=true)
      */
     private $responceHydration = [];
+
 
     public function getId(): ?int
     {
@@ -161,6 +191,38 @@ class Soap
     public function setResponceHydration(?array $responceHydration): self
     {
         $this->responceHydration = $responceHydration;
+
+        return $this;
+    }
+
+    public function getReponce(): ?string
+    {
+        return $this->reponce;
+    }
+
+    public function setReponce(string $reponce): self
+    {
+        $this->reponce = $reponce;
+
+        // Lets use this template to generate a skeleton
+        $xmlEncoder = new XmlEncoder(['xml_root_node_name' => 'soap:Envelope']);
+        $this->responceSkeleton = $xmlEncoder->decode($reponce);
+
+        return $this;
+    }
+
+    public function getRequest(): ?string
+    {
+        return $this->request;
+    }
+
+    public function setRequest(string $request): self
+    {
+        $this->request = $request;
+
+        // Lets use this template to generate a skeleton
+        $xmlEncoder = new XmlEncoder(['xml_root_node_name' => 'soap:Envelope']);
+        $this->requestSkeleton = $xmlEncoder->decode($request);
 
         return $this;
     }
