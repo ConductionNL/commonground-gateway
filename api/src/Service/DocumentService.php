85b3764499144cbdf7bbce08d2616782bf2f9685
Ruben van der Linde <ruben@conduction.nl>
2021-12-23 10:01:07 +0100

    Use toArray instead of serializer

--- api/src/Service/DocumentService.php
+++ api/src/Service/DocumentService.php
@@ -96,7 +96,7 @@ class DocumentService
         }
 
         // Lets do translations
-        $data = json_encode($data); // $this->serializer->serialize($data, 'json');
+        $data = json_encode($data->toArray()); // $this->serializer->serialize($data, 'json');
         $data = $this->translationService->parse($data, true, $translationVariables);
         $data = json_decode($data, true);
 
