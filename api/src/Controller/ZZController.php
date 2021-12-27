9de259fe0f2904f433bedf032a9f4f2e1cc9c8a9
Ruben van der Linde <ruben@conduction.nl>
2021-12-27 10:18:37 +0100

    Voorbeeld code voor nieuwe flow

--- api/src/Controller/ZZController.php
+++ api/src/Controller/ZZController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Document;
 use App\Service\DocumentService;
 use App\Service\EavService;
+use App\Service\EndpointService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -17,8 +18,14 @@ class ZZController extends AbstractController
      * @Route("/api/{entity}", name="dynamic_route_entity")
      * @Route("/api/{entity}/{id}", name="dynamic_route_collection")
      */
-    public function dynamicAction(?string $entity, ?string $id, Request $request, EavService $eavService, DocumentService $documentService, ValidationService $validationService): Response
+    public function dynamicAction(?string $entity, ?string $id,
+                                  Request $request,
+                                  EavService $eavService,
+                                  DocumentService $documentService,
+                                  ValidationService $validationService,
+                                  EndpointService $endpointService): Response
     {
+        // Below is hacky tacky
         $document = $this->getDoctrine()->getRepository('App:Document')->findOneBy(['route'=>$entity]);
         if ($document instanceof Document && $id) {
             return $documentService->handleDocument($document, $id);
@@ -26,7 +33,14 @@ class ZZController extends AbstractController
         if($entity == "postalCodes"){
             return $validationService->dutchPC4ToJson();
         }
+        // End of hacky tacky
 
+        // Let determine an endpoint (new way)
+        if($endpoint = $this->getDoctrine()->getRepository('App:Endpoint')->findOneBy(['route'=>$entity])){
+            return $endpointService->handleRequest($endpoint);
+        }
+
+        // Continue as normal (old way)
         return $eavService->handleRequest($request);
     }
 }
---
+++ api/src/Service/EndpointService.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Service;
+
+use ApiPlatform\Core\Exception\InvalidArgumentException;
+use App\Entity\Document;
+use App\Entity\File;
+use App\Entity\Endpoint;
+use Doctrine\ORM\EntityManagerInterface;
+use GuzzleHttp\Psr7\Request;
+use Psr\Http\Message\RequestInterface;
+use Ramsey\Uuid\Uuid;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class EndpointService
+{
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager, Request $request)
+    {
+        $this->entityManager = $entityManager;
+        $this->request = $request;
+    }
+
+    /**
+     * Get the data for a document and send it to the document creation service.
+     */
+    public function handleEndpoint(Endpoint $enpoint): Response
+    {
+        /* @todo endpoint toevoegen aan sessie */
+
+        // @tod creat logicdata, generalvaribales uit de translationservice
+
+        foreach($enpoint->getHandlers() as $handler){
+            // Check the JSON logic (voorbeeld van json logic in de validatie service)
+            if(){
+                return $this->handleHandler($handler);
+            }
+        }
+
+        // @todo we should end here so lets throw an error
+    }
+
+
+    /**
+     * Get the data for a document and send it to the document creation service.
+     */
+    public function handleHandler(Handler $handler): Response
+    {
+        /* @todo handler toevoegen aan sessie */
+
+        // Onderstaande zouden natuurlijk losse functies moeten zijn
+
+        // voorbeelden voor al deze dingen vind je in de soap service
+
+        // do mapping
+
+        // do translations (is nu nog een vaste array)
+
+        // all eav (if we have an entity) -> EAV service aantikken. 
+
+        // do translations
+
+        // do mapping
+
+        // create response (jatten de eav service)
+    }
+}
