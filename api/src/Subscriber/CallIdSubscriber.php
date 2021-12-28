78faa2b9ce5b1cd9efd57c7d39245dd7cd7cd63c
Barry Brands <barrybrands02@hotmail.com>
2021-12-28 09:53:54 +0100

    CallIdSubscriber small improvements

--- api/src/Subscriber/CallIdSubscriber.php
+++ api/src/Subscriber/CallIdSubscriber.php
@@ -2,17 +2,22 @@
 
 namespace App\Subscriber;
 
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
-use Symfony\Component\Security\Core\Exception\AccessDeniedException;
-use Symfony\Component\Serializer\SerializerInterface;
 use Ramsey\Uuid\Uuid;
 use Symfony\Component\HttpKernel\KernelEvents;
 use ApiPlatform\Core\EventListener\EventPriorities;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class CallIdSubscriber implements EventSubscriberInterface
 {
+    private SessionInterface $session;
+
+    public function __construct(SessionInterface $session)
+    {
+        $this->session = $session;
+    }
+
     // this method can only return the event names; you cannot define a
     // custom method name to execute when each event triggers
     public static function getSubscribedEvents()
@@ -24,8 +29,6 @@ class CallIdSubscriber implements EventSubscriberInterface
 
     public function OnFirstEvent(RequestEvent $event)
     {
-        if (!isset($_SESSION['callId'])) {
-            $_SESSION['callId'] = Uuid::uuid4()->toString();
-        }
+        !$this->session->get('callId') && $this->session->set('callId', Uuid::uuid4()->toString());
     }
 }
