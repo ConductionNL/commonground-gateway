0cac3b7037b4e57114ba0699ea247ec54f4f297e
Sarai Misidjan <Sarai@conduction.nl>
2021-12-29 11:11:34 +0100

    added logService

--- api/src/Subscriber/LogSubscriber.php
+++ api/src/Subscriber/LogSubscriber.php
@@ -3,23 +3,19 @@
 namespace App\Subscriber;
 
 use App\Entity\Log;
-use Doctrine\ORM\EntityManagerInterface;
-use ReflectionClass;
+use App\Service\LogService;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class LogSubscriber implements EventSubscriberInterface
 {
-    private EntityManagerInterface $entityManager;
-    private SessionInterface $session;
+    private LogService $logService;
 
-    public function __construct(EntityManagerInterface $entityManager, SessionInterface $session)
+    public function __construct(LogService $logService)
     {
-        $this->entityManager = $entityManager;
-        $this->session = $session;
+        $this->logService = $logService;
     }
 
     public static function getSubscribedEvents()
@@ -32,73 +28,8 @@ class LogSubscriber implements EventSubscriberInterface
     /**
      * @param ResponseEvent $event
      */
-    public function requestLog(ResponseEvent $event): Log
+    public function requestLog(ResponseEvent $event)
     {
-        $response = $event->getResponse();
-        $request = $event->getRequest();
-
-        $callLog = new Log();
-        $callLog->setType("in");
-        $callLog->setRequestMethod($request->getMethod());
-        $callLog->setRequestHeaders($request->headers->all());
-        $callLog->setRequestQuery($request->query->all());
-        $callLog->setRequestPathInfo($request->getPathInfo());
-        $callLog->setRequestLanguages($request->getLanguages());
-        $callLog->setRequestServer($request->server->all());
-        $callLog->setRequestContent($request->getContent());
-        // @todo get status
-        $callLog->setResponseStatus($this->getStatusWithCode($response->getStatusCode()) ?? null);
-        $callLog->setResponseStatusCode($response->getStatusCode());
-        $callLog->setResponseHeaders($response->headers->all());
-        $callLog->setResponseContent($response->getContent());
-
-        $routeName = $request->attributes->get('_route');
-        $routeParameters = $request->attributes->get('_route_params');
-        $callLog->setRouteName($routeName ?? null);
-        $callLog->setRouteParameters($routeParameters ?? null);
-
-        $now = new \DateTime();
-        $requestTime = $request->server->get('REQUEST_TIME');
-        $callLog->setResponseTime($now->getTimestamp() - $requestTime);
-        $callLog->setCreatedAt($now);
-
-        if ($this->session) {
-            // add before removing
-            $callLog->setCallId($this->session->get('callId',null));
-            $callLog->setSession($this->session->getId());
-            $callLog->setEndpoint($this->session->get('endpoint', null));
-            $callLog->setEntity($this->session->get('entity', null));
-            $callLog->setSource($this->session->get('source', null));
-            $callLog->setHandler($this->session->get('handler', null));
-
-            // remove before setting the session values
-            $this->session->remove('callId');
-            $this->session->remove('endpoint');
-            $this->session->remove('entity');
-            $this->session->remove('source');
-            $this->session->remove('handler');
-
-            // add session values
-            $callLog->setSessionValues($this->session->all());
-        }
-
-//        $this->entityManager->persist($callLog);
-//        $this->entityManager->flush();
-
-        return $callLog;
-    }
-
-    private function getStatusWithCode(int $statusCode): ?string
-    {
-        $reflectionClass = new ReflectionClass(Response::class);
-        $constants = $reflectionClass->getConstants();
-
-        foreach ($constants as $status => $value) {
-            if ($value == $statusCode) {
-                return $status;
-            }
-        }
-
-        return null;
+        $this->logService->createLog($event);
     }
 }
