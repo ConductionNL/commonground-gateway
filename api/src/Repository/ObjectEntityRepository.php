eb96fb5f0fac1436040dbb80c9adcff137006645
Wilco Louwerse <wilco@conduction.nl>
2022-01-14 09:51:40 +0100

    remove todo comment

--- api/src/Repository/ObjectEntityRepository.php
+++ api/src/Repository/ObjectEntityRepository.php
@@ -94,7 +94,7 @@ class ObjectEntityRepository extends ServiceEntityRepository
     {
         $result = [];
         foreach ($array as $key=>$value) {
-            $key = str_replace(['_', '..'], ['.', '._'], $key); //@TODO: test if this works correctly
+            $key = str_replace(['_', '..'], ['.', '._'], $key); 
             if (substr($key, 0, 1) == '.') {
                 $key = '_'.ltrim($key, $key[0]);
             }
@@ -187,7 +187,7 @@ class ObjectEntityRepository extends ServiceEntityRepository
     }
 
     //todo: typecast?
-
+    //todo: remove?
     private function buildFilter(QueryBuilder $query, $filters, $prefix = 'o', $level = 0): QueryBuilder
     {
         $query->leftJoin($prefix.'.objectValues', $level.'.objectValues');
