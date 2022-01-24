10e474aa7f7293efe292fa8dd8c01fa6e0ccc65e
Sarai Misidjan <Sarai@conduction.nl>
2022-01-24 14:26:03 +0100

    getTableNames

--- api/src/Subscriber/TranslationTablesSubscriber.php
+++ api/src/Subscriber/TranslationTablesSubscriber.php
@@ -3,6 +3,7 @@
 namespace App\Subscriber;
 
 use ApiPlatform\Core\EventListener\EventPriorities;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ViewEvent;
@@ -10,15 +11,17 @@ use Symfony\Component\HttpKernel\KernelEvents;
 
 class TranslationTablesSubscriber implements EventSubscriberInterface
 {
+    private EntityManagerInterface $entityManager;
 
-    public function __construct()
+    public function __construct(EntityManagerInterface $entityManager)
     {
+        $this->entityManager = $entityManager;
     }
 
     public static function getSubscribedEvents()
     {
         return [
-            KernelEvents::VIEW => ['translationTablesNames', EventPriorities::PRE_VALIDATE],
+            KernelEvents::VIEW => ['translationTablesNames', EventPriorities::PRE_SERIALIZE],
         ];
     }
 
@@ -30,12 +33,14 @@ class TranslationTablesSubscriber implements EventSubscriberInterface
         $route = $event->getRequest()->attributes->get('_route');
         $path = $event->getRequest()->getPathInfo();
 
-        if (
-            $route !== 'api_translations_get_collection' && $path !== '/admin/getTranslationTables'
-        ) {
-            return;
+        if ($route !== 'api_translations_get_collection' && $path !== '/admin/table_names') {
+            return false;
         }
-        
-       return;
+
+        $translationsRepo = $this->entityManager->getRepository('App:Translation');
+
+        $event->setResponse(new Response(json_encode([
+            'results' => $translationsRepo->getTables()
+        ]), 200, ['Content-type' => 'application/json']));
     }
 }
