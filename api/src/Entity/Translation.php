c61da350715be704bfcfdc51a11efd6955bcde11
Sarai Misidjan <Sarai@conduction.nl>
2022-01-24 14:25:41 +0100

    fix path name

--- api/src/Entity/Translation.php
+++ api/src/Entity/Translation.php
@@ -23,7 +23,7 @@ use Symfony\Component\Validator\Constraints as Assert;
  *  },
  *  collectionOperations={
  *      "get"={"path"="/admin/translations"},
- *      "get"={"path"="/admin/getTranslationTables"},
+ *      "get"={"path"="/admin/table_names"},
  *      "post"={"path"="/admin/translations"}
  *  })
  * )
