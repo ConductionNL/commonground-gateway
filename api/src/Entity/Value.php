360aceb049e63bf288dad3f47a5eb602c383bd50
Sarai Misidjan <Sarai@conduction.nl>
2022-01-31 15:58:21 +0100

    added routes

--- api/src/Entity/Value.php
+++ api/src/Entity/Value.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Annotation\ApiResource;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\BooleanFilter;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\DateFilter;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\OrderFilter;
@@ -25,6 +26,18 @@ use Symfony\Component\Validator\Constraints as Assert;
  *
  * @category Entity
  *
+ * @ApiResource(
+ *  normalizationContext={"groups"={"read"}, "enable_max_depth"=true},
+ *  denormalizationContext={"groups"={"write"}, "enable_max_depth"=true},
+ *  itemOperations={
+ *      "get"={"path"="/admin/values/{id}"},
+ *      "put"={"path"="/admin/values/{id}"},
+ *      "delete"={"path"="/admin/values/{id}"}
+ *  },
+ *  collectionOperations={
+ *      "get"={"path"="/admin/values"},
+ *      "post"={"path"="/admin/values"}
+ *  })
  * @ORM\Entity(repositoryClass="App\Repository\ValueRepository")
  * @Gedmo\Loggable(logEntryClass="Conduction\CommonGroundBundle\Entity\ChangeLog")
  *
