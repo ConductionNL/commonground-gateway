330107c2b4f6fbb31ab3ab0ec4c208ee23bcc7f5
Wilco Louwerse <wilco@conduction.nl>
2022-02-03 16:52:56 +0100

    cleanup

--- api/src/Service/ValidaterService.php
+++ api/src/Service/ValidaterService.php
@@ -27,10 +27,13 @@ class ValidaterService
 
     public function validateData(array $data, Entity $entity)
     {
-        // @todo if monday is posted as 5 (int) it still is a string here....
+        // @todo if weer.monday is posted as 5 (int) it still is a string here....
+        // todo: But if we do the same for person.givenName it is an integer here
         // dump(is_int($data['monday']));
         // $data['monday'] = 5;
         $validator = $this->getEntityValidator($entity);
+
+        // TODO: what if we have fields in $data that do not exist on this Entity?
 
         try {
             $validator->assert($data);
@@ -120,7 +123,7 @@ class ValidaterService
                     return new Rules\ArrayType();
                 }
                 return new Rules\ArrayType();
-//                return new Rules\ObjectType(); // ObjectType expect an actual class object not a json object, so this will not work...
+//                return new Rules\ObjectType(); // TODO: ObjectType expects an actual class object not a json object, so this will not work...
             default:
                 throw new GatewayException('Unknown attribute type!', null, null, ['data' => $attribute->getType(), 'path' => $attribute->getEntity()->getName().'.'.$attribute->getName(), 'responseType' => Response::HTTP_BAD_REQUEST]);
         }
@@ -244,100 +247,5 @@ class ValidaterService
         }
 
         return $attributeValidator;
-    }
-
-
-    // Example code:
-    // TODO: translate / move this code below to functions above^
-    // TODO: remove functions below after... >>>
-
-    private function createEntityValidator(array $data, Entity $entity, string $method, Validator $validator)
-    {
-        // Lets validate each attribute
-        foreach ($entity->getAttributes() as $attribute) {
-            // fallback for empty data
-            !array_key_exists($attribute->getName(), $data) && $data[$attribute->getName()] = null;
-
-            $validator->key($attribute->getName(), $this->createAttributeEntityValidator($data, $attribute, $method, $validator));
-            //
-            // Lets clean it up
-            unset($data[$attribute->getName()]);
-        }
-
-        // Lets see if we have attributes that should not be here (if we haven’t cleaned it up it isn’t an attribute)
-        foreach ($data as $key => $value) {
-            $validator->key(
-                $key,
-            /** custom not allowed validator*/
-            );
-        }
-
-        return $validator;
-    }
-
-    private function createAttributeEntityValidator(array $data, Attribute $attribute, string $method, Validator $validator)
-    {
-        // if this is an entity we can skip al this
-        if ($attribute->getType() === 'object' || $attribute->getType() === null) {
-            // @todo maybe error?
-            return $validator;
-        }
-
-        // Validate type
-        // kijk naar de huidige validations service on validateType()
-
-        // Let be a bit compasionate and compatable
-        $type = str_replace(['integer', 'boolean', 'text'], ['int', 'bool', 'string'], $attribute->getType());
-        // In order not to allow any respect/validation function to be called we explicatly call those containing formats
-        $basicTypes = ['bool', 'string', 'int', 'array', 'float'];
-        // new route
-        if (in_array($type, $basicTypes)) {
-            $validator->type($type);
-        } else {
-            // The are some uncoverd types so we will have to add those manualy
-            switch ($type) {
-                case 'date':
-                    $validator->date();
-                    break;
-                case 'datetime':
-                    $validator->dateTime();
-                    break;
-                case 'number':
-                    $validator->numericVal();
-                    break;
-                case 'object':
-                    // We dont validate an object normaly but hand it over to its own validator
-                    $this->validate($data[$attribute->getName()]);
-                    break;
-                default:
-                    // we should never end up here
-                    /* @todo throw an custom error */
-            }
-        }
-
-        // Validate format
-        // kijk naar de huidige validations service on validateType()
-
-        // Besides the type and format there could be other validations (like minimal datetime, requered etc)
-        foreach ($attribute->getValidations() as $key => $value) {
-            switch ($key) {
-                    // first we need to do of casses (anything not natively supported by validator or requiring additional logic)
-                case 'jsonlogic':
-                    // code to be executed if n=label1;
-                    break;
-                case 'postalcode':
-                    // code to be executed if n=label2;
-                    break;
-                case 'label3':
-                    // code to be executed if n=label3;
-                    break;
-                    // what is then left is the generic stuff
-                default:
-                    // we should not end up here…
-                    // @todo throw error
-            }
-        }
-
-        return $validator;
     }
 }
