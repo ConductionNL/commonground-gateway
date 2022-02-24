642ebed8225a3f5a60aa63158a820047c34101ba
Wilco Louwerse <wilco@conduction.nl>
2022-02-24 14:58:27 +0100

    Better validation for Base64String Mime type

--- api/src/Service/Validation/Exceptions/Base64StringException.php
+++ api/src/Service/Validation/Exceptions/Base64StringException.php
@@ -8,10 +8,10 @@ final class Base64StringException extends ValidationException
 {
     protected $defaultTemplates = [
         self::MODE_DEFAULT => [
-            self::STANDARD => '{{name}} must be a base64-encoded string.',
+            self::STANDARD => '{{name}} must be a base64-encoded string. {{exceptionMessage}}',
         ],
         self::MODE_NEGATIVE => [
-            self::STANDARD => '{{name}} must not be a base64-encoded string.',
+            self::STANDARD => '{{name}} must not be a base64-encoded string. {{exceptionMessage}}',
         ],
     ];
 }
--- api/src/Service/Validation/Rules/Base64String.php
+++ api/src/Service/Validation/Rules/Base64String.php
@@ -4,10 +4,18 @@ namespace App\Service\Validation\Rules;
 
 use Exception;
 use Respect\Validation\Rules\AbstractRule;
+use Respect\Validation\Validatable;
 use Respect\Validation\Validator;
 
 final class Base64String extends AbstractRule
 {
+    /**
+     * Used for Base64StringException
+     *
+     * @var string|null
+     */
+    private ?string $exceptionMessage;
+
     /**
      * @inheritDoc
      */
@@ -16,6 +24,7 @@ final class Base64String extends AbstractRule
         // Example of how input should look like: data:text/plain;base64,ZGl0IGlzIGVlbiB0ZXN0IGRvY3VtZW50
         // Make sure we have a string as input
         if (!Validator::stringType()->validate($input)) {
+            $this->setExceptionMessage("This is not a string.");
             return false;
         }
 
@@ -23,13 +32,22 @@ final class Base64String extends AbstractRule
         $exploded_input = explode(',', $input);
         $base64 = end($exploded_input);
         if (!Validator::base64()->validate($base64)) {
+            $this->setExceptionMessage("This is not a valid base64.");
             return false;
         }
 
         try {
+            $mimeType1 = false;
             // Get mimeType from the base64 input string and compare it with the mimeType we get from using the base64 to open a file and get the mimeType
-            //todo
-            $mimeType1 = 'todo';
+            // If input string has a single comma (',') in it count($exploded_input) will return 2
+            if (count($exploded_input) == 2) {
+                // Take the part before the comma (',') and look for the mimeType in it
+                $matchesCount = preg_match('/data:([\w\/]+);base64/', $exploded_input[0], $matches);
+                if ($matchesCount == 1) {
+                    // We found a mimeType in the base64 input string
+                    $mimeType1 = $matches[1];
+                }
+            }
 
             // Use the base64 to open a file and get the mimeType
             $fileData = base64_decode($base64);
@@ -37,16 +55,34 @@ final class Base64String extends AbstractRule
             $mimeType2 = finfo_buffer($f, $fileData, FILEINFO_MIME_TYPE);
             finfo_close($f);
 
-//            if ($mimeType1 !== $mimeType2) {
-//                return false;
-//            }
+            if ($mimeType1 !== $mimeType2) {
+                $this->setExceptionMessage("Mime type mismatch: ".$mimeType1." should match: ".$mimeType2);
+                return false;
+            }
         } catch (Exception $exception) {
-//            var_dump(3);
+            $this->setExceptionMessage($exception->getMessage());
             return false;
         }
-//        var_dump($mimeType2);
-//
-//        var_dump(5);
+
         return true;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getExceptionMessage(): ?string
+    {
+        return $this->exceptionMessage;
+    }
+
+    /**
+     * @param string $exceptionMessage
+     * @return Validatable
+     */
+    public function setExceptionMessage(string $exceptionMessage): Validatable
+    {
+        $this->exceptionMessage = $exceptionMessage;
+
+        return $this;
     }
 }
