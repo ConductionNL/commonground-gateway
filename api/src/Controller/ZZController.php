04d79731d09870a59c505500e8f7912a5296b4b9
Ruben van der Linde <ruben@conduction.nl>
2022-03-24 07:34:04 +0100

    Route and variabale fixes in conntroller

--- api/src/Controller/ZZController.php
+++ api/src/Controller/ZZController.php
@@ -19,19 +19,10 @@ use Symfony\Component\Serializer\SerializerInterface;
 class ZZController extends AbstractController
 {
     /**
-     * @Route("/api/{path}", name="dynamic_route_entity")
-     * @Route("/api/{path}/{subPath2}")
-     * @Route("/api/{path}/{subPath2}/{subPath3}")
-     * @Route("/api/{path}/{subPath2}/{subPath3}/{subPath4}")
-     * @Route("/api/{entity}/{id}", name="dynamic_route_collection")
+     * @Route("/api/{path}", name="dynamic_route", requirements={"path" = ".+"})
      */
     public function dynamicAction(
         ?string $path,
-        ?string $subPath2,
-        ?string $subPath3,
-        ?string $subPath4,
-        ?string $entity,
-        ?string $id,
         Request $request,
         EavService $eavService,
         DocumentService $documentService,
@@ -43,6 +34,7 @@ class ZZController extends AbstractController
 
 
         // Below is hacky tacky
+        // @todo refactor
         $document = $this->getDoctrine()->getRepository('App:Document')->findOneBy(['route' => $entity]);
         if ($document instanceof Document && $id) {
             return $documentService->handleDocument($document, $id);
@@ -53,67 +45,60 @@ class ZZController extends AbstractController
         // End of hacky tacky
 
         // Get full path
-        // @TODO wont work with more than 4 subpaths
-        $fullPath = '';
-        isset($path) && $fullPath  .= '/' . $path;
-        isset($subPath2) && $fullPath .= '/' . $subPath2;
-        isset($subPath3) && $fullPath .= '/' . $subPath3;
-        isset($subPath4) && $fullPath .= '/' . $subPath4;
-      
+        // We should look at a better search moddel in sql
         $allEndpoints = $this->getDoctrine()->getRepository("App:Endpoint")->findAll();
 
         // Match path to regex of Endpoints
-        foreach ($allEndpoints as $endpoint) {
-            if ($endpoint->getPathRegex() !== null && preg_match($endpoint->getPathRegex(), $fullPath)) {
-                $matchedEndpoint = $endpoint;
+        foreach ($allEndpoints as $currentEndpoint) {
+            if ($currentEndpoint->getPathRegex() !== null && preg_match($currentEndpoint->getPathRegex(), $path)) {
+                $endpoint = $currentEndpoint;
                 break;
-            }    
-        }
-      
-        // Let determine an endpoint (new way)
-        if (isset($matchedEndpoint)) {
-            
-            // Create array for filtering (in progress, should be moved to the correct service)
-            $filterArray = [];
-            $fullPathArray = array_values(array_filter(explode('/', $fullPath)));
-            foreach ($matchedEndpoint->getPath() as $key => $pathPart) {
-                if (substr($pathPart, 0)[0] == '{') {
-                    $variable = str_replace('{', '', $pathPart);
-                    $variable = str_replace('}', '', $variable);
-                    $filterArray[$variable] = $fullPathArray[$key];
-                }    
             }
-            var_dump($filterArray);die;
+        }
 
+        // @todo exit here if we do not have an endpoint
 
-            // Try handler proces and catch exceptions
+        // Let create the variable
+
+        // Create array for filtering (in progress, should be moved to the correct service)
+        $variables = [];
+        $pathArray = array_values(array_filter(explode('/', $path)));
+        foreach ($endpoint->getPath() as $key => $pathPart) {
+            // Let move path parts that are defined as variables to the filter array
+            if (array_key_exists($key, $pathArray)) {
+                $variables[$pathPart] = $pathArray[$key];
+            }
+        }
+
+        // Lets add the query parameters to the variables
+        $variables = array_merge_recursive($variables, $request->query->all()) ;
+        // Lets get all the post variables
+        $variables = array_merge_recursive($variables, $request->request->all()) ;
+
+        // Try handler proces and catch exceptions
+        try {
+            return $handlerService->handleEndpoint($endpoint, $variables);
+        } catch (GatewayException $gatewayException) {
+            $options = $gatewayException->getOptions();
+            $acceptType = $handlerService->getRequestType('accept');
+
             try {
-                return $handlerService->handleEndpoint($matchedEndpoint);
-            } catch (GatewayException $gatewayException) {
-                $options = $gatewayException->getOptions();
-                $acceptType = $handlerService->getRequestType('accept');
-
-                try {
-                    $response = new Response(
-                        $serializer->serialize(['message' =>  $gatewayException->getMessage(), 'data' => $options['data'], 'path' => $options['path']], $acceptType),
-                        $options['responseType'] ?? Response::HTTP_INTERNAL_SERVER_ERROR,
-                        ['content-type' => $acceptType]
-                    );
-                    // Catch NotEncodableValueException from symfony serializer
-                } catch (NotEncodableValueException $e) {
-                    $response = new Response(
-                        $serializer->serialize(['message' =>  $gatewayException->getMessage(), 'data' => $options['data'], 'path' => $options['path']], 'json'),
-                        $options['responseType'] ?? Response::HTTP_INTERNAL_SERVER_ERROR,
-                        ['content-type' => 'json']
-                    );
-                }
-                $logService->saveLog($request, $response);
-
-                return $response->prepare($request);
+                $response = new Response(
+                    $serializer->serialize(['message' =>  $gatewayException->getMessage(), 'data' => $options['data'], 'path' => $options['path']], $acceptType),
+                    $options['responseType'] ?? Response::HTTP_INTERNAL_SERVER_ERROR,
+                    ['content-type' => $acceptType]
+                );
+                // Catch NotEncodableValueException from symfony serializer
+            } catch (NotEncodableValueException $e) {
+                $response = new Response(
+                    $serializer->serialize(['message' =>  $gatewayException->getMessage(), 'data' => $options['data'], 'path' => $options['path']], 'json'),
+                    $options['responseType'] ?? Response::HTTP_INTERNAL_SERVER_ERROR,
+                    ['content-type' => 'json']
+                );
             }
-        }
+            $logService->saveLog($request, $response);
 
-        // Continue as normal (old way)
-        return $eavService->handleRequest($request);
+            return $response->prepare($request);
+        }
     }
 }
