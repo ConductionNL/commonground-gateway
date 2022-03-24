088f4dfd0858895ed473492b3cde15e98cfc2a3b
Wilco Louwerse <wilco@conduction.nl>
2022-03-24 13:45:35 +0100

    remove filter for old property

--- api/src/Entity/Entity.php
+++ api/src/Entity/Entity.php
@@ -42,7 +42,6 @@ use Symfony\Component\Validator\Constraints as Assert;
  * @ApiFilter(OrderFilter::class)
  * @ApiFilter(DateFilter::class, strategy=DateFilter::EXCLUDE_NULL)
  * @ApiFilter(SearchFilter::class, properties={
- *     "type": "exact",
  *     "name": "exact"
  * })
  */
