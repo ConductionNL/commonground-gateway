a071de69974e5796cd3d8451b30a7550b2e4a000
Robert Zondervan <robert@conduction.nl>
2022-04-01 15:09:17 +0200

    Remove unused uses

--- api/src/Controller/ConvenienceController.php
+++ api/src/Controller/ConvenienceController.php
@@ -7,14 +7,12 @@ use App\Service\OasParserService;
 use App\Service\ParseDataService;
 use App\Service\PubliccodeService;
 use Doctrine\ORM\EntityManagerInterface;
-use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
-use Symfony\Component\Yaml\Yaml;
 
 class ConvenienceController extends AbstractController
 {
