65d5522e0710ff80fd2dd2dfb402b6d01bc2b017
Wilco Louwerse <wilco@conduction.nl>
2022-04-04 16:40:54 +0200

    Set owner on subresources when doing a post

--- api/src/Service/ValidationService.php
+++ api/src/Service/ValidationService.php
@@ -502,6 +502,7 @@ class ValidationService
                 }
 
                 $subObject = $this->validateEntity($subObject, $object);
+                $this->objectEntityService->handleOwner($subObject); // Do this after all CheckAuthorization function calls
 
                 // We need to persist if this is a new ObjectEntity in order to set and getId to generate the uri...
                 $this->em->persist($subObject);
@@ -1002,6 +1003,7 @@ class ValidationService
                     $subObject = $valueObject->getValue();
                 }
                 $subObject = $this->validateEntity($subObject, $value);
+                $this->objectEntityService->handleOwner($subObject); // Do this after all CheckAuthorization function calls
                 $this->em->persist($subObject);
 
                 // If no errors we can push it into our object
