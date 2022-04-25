80eef7782b1c4f8b0c0086a3c3bce0cda097ef25
Wilco Louwerse <wilco@conduction.nl>
2022-04-25 11:00:59 +0200

    Lets throw an error if we can't find a session

--- api/src/Security/UserTokenAuthenticator.php
+++ api/src/Security/UserTokenAuthenticator.php
@@ -219,6 +219,10 @@ class UserTokenAuthenticator extends AbstractGuardAuthenticator
         $user = $this->commonGroundService->getResource(['component' => 'uc', 'type' => 'users', 'id' => $payload['userId']], [], false, false, true, false, false);
         $session = $this->commonGroundService->getResource(['component' => 'uc', 'type' => 'sessions', 'id' => $payload['session']], [], false, false, true, false, false);
 
+        if ($session === false) {
+            throw new AuthenticationException('The provided token refers to an unknown (and thus, invalid) session');
+        }
+
         $this->checkExpiry($session);
 
         return $this->getUcUser($user);
