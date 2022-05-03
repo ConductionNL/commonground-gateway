832b443c861e7f4dfb630fbbab9f63882fe06035
Robert Zondervan <robert@conduction.nl>
2022-05-03 10:08:36 +0200

    Delete from cache after deletion

--- api/src/Service/ObjectEntityService.php
+++ api/src/Service/ObjectEntityService.php
@@ -479,7 +479,7 @@ class ObjectEntityService
                 break;
             case 'DELETE':
                 // delete object
-
+                $this->functionService->removeResultFromCache($object);
                 //todo: -start- old code...
                 //TODO: old code for deleting an ObjectEntity
 
