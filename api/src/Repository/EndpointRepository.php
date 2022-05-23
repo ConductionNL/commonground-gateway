15f4154ac82c378b78376911784d5b1d5c0db457
Sarai Misidjan <Sarai@conduction.nl>
2022-05-23 16:09:11 +0200

    added findByApplication function

--- api/src/Repository/EndpointRepository.php
+++ api/src/Repository/EndpointRepository.php
@@ -2,7 +2,9 @@
 
 namespace App\Repository;
 
+use App\Entity\Application;
 use App\Entity\Endpoint;
+use App\Entity\Entity;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\ORM\NonUniqueResultException;
 use Doctrine\Persistence\ManagerRegistry;
@@ -53,6 +55,23 @@ class EndpointRepository extends ServiceEntityRepository
           ->setMaxResults(1)
           ->getQuery()
           ->getResult();
+    }
+
+    /**
+     * @param string $application  The Application.
+     * @return Endpoint Returns a Endpoint object
+     */
+    public function findByApplication(Application $application): array
+    {
+        $query = $this->createQueryBuilder('e')
+            ->leftJoin('e.applications', 'a')
+            ->where('a.id = :applicationId')
+            ->setParameter('applicationId', $application->getId()->toString())
+            ->distinct();
+
+        return $query
+            ->getQuery()
+            ->getResult();
     }
 
     // /**
