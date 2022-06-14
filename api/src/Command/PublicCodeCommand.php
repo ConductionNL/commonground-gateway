90d494a1601871e9e5a85bc5b2679118246f5acb
Barry Brands <barrybrands02@hotmail.com>
2022-06-14 13:35:34 +0200

    PublicCode command added

---
+++ api/src/Command/PublicCodeCommand.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use GuzzleHttp\Client;
+use Symfony\Component\Yaml\Exception\ParseException;
+use Symfony\Component\Yaml\Yaml;
+use App\Entity\CollectionEntity;
+use Doctrine\ORM\EntityManagerInterface;
+use GuzzleHttp\Exception\GuzzleException;
+use App\Service\OasParserService;
+use App\Service\ParseDataService;
+
+// the name of the command is what users type after "php bin/console"
+#[AsCommand(name: 'app:load-publiccodes')]
+class PublicCodeCommand extends Command
+{
+    protected static $defaultName = 'app:load-publiccodes';
+    protected ParameterBagInterface $parameterBag;
+    protected EntityManagerInterface $entityManager;
+    protected OasParserService $oasParser;
+    protected ParseDataService $dataService;
+
+    public function __construct(
+        ParameterBagInterface $parameterBag,
+        EntityManagerInterface $entityManager,
+        ParseDataService $dataService
+    ) {
+        $this->parameterBag = $parameterBag;
+        $this->entityManager = $entityManager;
+        $this->oasParser = new OasParserService($entityManager);
+        $this->dataService = $dataService;
+
+        parent::__construct();
+    }
+
+
+    protected function configure(): void
+    {
+        $this
+            // the command help shown when running the command with the "--help" option
+            ->setHelp('This command allows you to load a publiccode as a collection and configure it on this gateway.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        // ... put here the code to create the user
+        $publicCodeLinks = $this->parameterBag->get('PUBLICCODE');
+        $publicCodeLinks = explode(',', $publicCodeLinks);
+
+        $httpClient = new Client(['base_uri' => '']);
+        foreach ($publicCodeLinks as $link) {
+            $this->loadPublicCode($link, $httpClient, $output);
+        }
+
+        // this method must return an integer number with the "exit status code"
+        // of the command. You can also use these constants to make code more readable
+
+        // return this if there was no problem running the command
+        // (it's equivalent to returning int(0))
+        return Command::SUCCESS;
+
+        // or return this if some error happened during the execution
+        // (it's equivalent to returning int(1))
+        // return Command::FAILURE;
+
+        // or return this to indicate incorrect command usage; e.g. invalid options
+        // or missing arguments (it's equivalent to returning int(2))
+        // return Command::INVALID
+    }
+
+
+
+    protected function loadPublicCode(string $publicCodeLink, Client $httpClient, OutputInterface $output)
+    {
+        try {
+            $httpResponse = $httpClient->request('GET', trim($publicCodeLink, ' '));
+        } catch (GuzzleException $exception) {
+            $output->writeln([
+                '',
+                $publicCodeLink,
+                $exception->getMessage(),
+                ''
+            ]);
+            return Command::FAILURE;
+        }
+        try {
+            $publicCodeParsed = Yaml::parse($httpResponse->getBody()->getContents());
+        } catch (ParseException $exception) {
+            $output->writeln([
+                '',
+                $exception->getMessage(),
+                ''
+            ]);
+            return Command::FAILURE;
+        }
+        // Prevent Collection from being made again
+        if (isset($publicCodeParsed['description']['en']['apiDocumentation'])) {
+            $duplicatedCollections = $this->entityManager->getRepository(CollectionEntity::class)->findBy(['locationOAS' => $publicCodeParsed['description']['en']['apiDocumentation']]);
+            if (count($duplicatedCollections) > 0) {
+                return;
+            }
+        }
+
+        $collection = new CollectionEntity();
+        $collection->setName($publicCodeParsed['name']);
+        isset($publicCodeParsed['description']['en']['shortDescription']) && $collection->setDescription($publicCodeParsed['description']['en']['shortDescription']);
+        $collection->setSourceType('GitHub');
+        $collection->setSourceUrl($publicCodeLink);
+        isset($publicCodeParsed['description']['en']['apiDocumentation']) && $collection->setLocationOAS($publicCodeParsed['description']['en']['apiDocumentation']);
+        $collection->setLoadTestData(isset($publicCodeParsed['description']['en']['testDataLocation']));
+        isset($publicCodeParsed['description']['en']['testDataLocation']) && $collection->setTestDataLocation($publicCodeParsed['description']['en']['testDataLocation']);
+
+        $this->entityManager->persist($collection);
+        $this->entityManager->flush();
+
+
+        $collection = $this->oasParser->parseOas($collection);
+        $message = 'Succesfully created collection ' . $publicCodeParsed['name'] . ' and config loaded';
+        if ($collection->getLoadTestData()) {
+            $this->dataService->loadData($collection->getTestDataLocation(), $collection->getLocationOAS(), true);
+            $message .= ' with testdata';
+        };
+
+        $collection = $this->entityManager->getRepository(CollectionEntity::class)->find($collection->getId());
+        $collection->setSyncedAt(new \DateTime('now'));
+        $this->entityManager->persist($collection);
+        $this->entityManager->flush();
+
+
+        $output->writeln([
+            '',
+            $message,
+            ''
+        ]);
+    }
+}
