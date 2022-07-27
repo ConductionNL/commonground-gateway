44e3bf549e1ffde36b30a9ce39a393700dc15044
Barry Brands <barrybrands02@hotmail.com>
2022-07-27 16:09:21 +0200

    CollectionPersistSubscriber update endpoint paths added

--- api/src/EventListener/CollectionPersistSubscriber.php
+++ api/src/EventListener/CollectionPersistSubscriber.php
@@ -3,9 +3,11 @@
 namespace App\EventListener;
 
 use App\Entity\CollectionEntity;
+use App\Entity\ObjectEntity;
 use App\Service\OasParserService;
 use App\Service\ParseDataService;
 use Doctrine\Bundle\DoctrineBundle\EventSubscriber\EventSubscriberInterface;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Events;
 use Doctrine\Persistence\Event\LifecycleEventArgs;
@@ -62,6 +64,9 @@ class CollectionPersistSubscriber implements EventSubscriberInterface
         $collection = $this->entityManager->getRepository(CollectionEntity::class)->find($object->getId());
 
         if ($collection->getSyncedAt()) {
+            if ($collection->getPrefix() && $collection->getEndpoints()) {
+                $this->updateEndpointPaths($collection->getEndpoints(), $collection->getPrefix());
+            }
             return;
         }
         if (!$collection->getLocationOAS()) {
@@ -71,6 +76,23 @@ class CollectionPersistSubscriber implements EventSubscriberInterface
             return;
         }
 
+        $this->loadCollection($collection, $object);
+    }
+
+
+    private function updateEndpointPaths(Collection $endpoints, string $prefix)
+    {
+        foreach ($endpoints as $endpoint) {
+            $path = $endpoint->getPath();
+            array_unshift($path, $prefix);
+            $endpoint->setPath($path);
+            $this->entityManager->persist($endpoint);
+        }
+        $this->entityManager->flush();
+    }
+
+    private function loadCollection(CollectionEntity $collection, object $object)
+    {
         $collection = $this->oasParser->parseOas($collection);
         $collection->getLoadTestData() ? $this->dataService->loadData($collection->getTestDataLocation(), $collection->getLocationOAS(), true) : null;
 
