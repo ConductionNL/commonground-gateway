fcdb8b3fbfcd7aa0ba4741fe0f85bb018b60d826
Sarai Misidjan <sarai@conduction.nl>
2022-07-27 16:11:33 +0200

    fix throws
    added as enum, needs to be dynamic

--- api/src/Entity/Cronjob.php
+++ api/src/Entity/Cronjob.php
@@ -100,6 +100,7 @@ class Cronjob
     /**
      * @var array The actions that put on the stack by the crontab.
      *
+     * @Assert\Choice({"commongateway.handler.pre", "commongateway.handler.post"}, multiple=true)
      * @Groups({"read", "write"})
      * @ORM\Column(type="array")
      */
