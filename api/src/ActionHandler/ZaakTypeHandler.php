f5a15d9118dc1ade85a28f778ed5b2469f005475
Sarai Misidjan <sarai@conduction.nl>
2022-07-29 13:19:56 +0200

    fix zaaktype handler

--- api/src/ActionHandler/ZaakTypeHandler.php
+++ api/src/ActionHandler/ZaakTypeHandler.php
@@ -2,9 +2,7 @@
 
 namespace App\ActionHandler;
 
-use App\Entity\Action;
 use App\Entity\ObjectEntity;
-use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Serializer\Encoder\XmlEncoder;
@@ -24,19 +22,14 @@ class ZaakTypeHandler implements ActionHandlerInterface
         $data = $xmlEncoder->decode($request->getContent(), 'xml');
         $dotData = new \Adbar\Dot($data);
 
-        // getConfiguration does nog work
         return $dotData->get($configuration['identifierPath']);
     }
 
     public function __run(array $data, array $configuration): array
     {
-//        return $data;
         $identifier = $this->getIdentifier($data['request'], $configuration);
-        var_dump($identifier);
-
-        $entity = $this->entityManager->getRepository("App:Entity")->findOneBy(["id" => "cf294a75-6cf2-4c3d-80a5-2c9a4caaae77"]);
+        $entity = $this->entityManager->getRepository("App:Entity")->findOneBy(["id" => $configuration['entityId']]);
         $objectEntities = $this->entityManager->getRepository("App:ObjectEntity")->findByEntity($entity, ['identificatie' => $identifier]);
-        var_dump(count($objectEntities));
 
         if(count($objectEntities) > 0 && $objectEntities[0] instanceof ObjectEntity){
             $objectEntity = $objectEntities[0];
@@ -44,13 +37,6 @@ class ZaakTypeHandler implements ActionHandlerInterface
             $url = $objectEntity->getValueByAttribute($objectEntity->getEntity()->getAttributeByName('url'))->getStringValue();
             $data['result'] = $url;
         }
-
-
-        // find object entity with entity zaaktype
-        // get omschrijving code
-        // aan de hand van de code de casetype zoeken -> identificatie
-
-        var_dump('running the ZaakTypeHandler!');
 
         return $data;
     }
