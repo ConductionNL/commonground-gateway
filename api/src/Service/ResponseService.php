19e75d2c5495654a714ab3e6a9118a701599b4f0
Wilco Louwerse <wilco@conduction.nl>
2022-08-02 15:32:35 +0200

    update note

--- api/src/Service/ResponseService.php
+++ api/src/Service/ResponseService.php
@@ -165,7 +165,7 @@ class ResponseService
             )
         );
         if ($item->isHit() && !$dateRead) {
-//            var_dump('FromCache: '.$result->getId().http_build_query($fields ?? [],'',','));
+//            var_dump('FromCache: '.$result->getId().'acceptType_'.$acceptType.'level_'.$level.'fields_'.http_build_query($fields ?? [], '', ',').'extend_'.http_build_query($extend ?? [], '', ','));
             return $this->filterResult($item->get(), $result, $skipAuthCheck);
         }
         $item->tag('object_'.base64_encode($result->getId()->toString()));
