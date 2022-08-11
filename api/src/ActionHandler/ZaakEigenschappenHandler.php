a09051417a8446032695ed15fe65565d974ba41c
Sarai Misidjan <sarai@conduction.nl>
2022-08-11 14:14:46 +0200

    zaakeigenschappen plugin
    need to fix the usage of the objectEntityService and the validatorService

--- api/src/ActionHandler/ZaakEigenschappenHandler.php
+++ api/src/ActionHandler/ZaakEigenschappenHandler.php
@@ -3,18 +3,28 @@
 namespace App\ActionHandler;
 
 use App\Entity\ObjectEntity;
+use App\Service\ObjectEntityService;
+use App\Service\ValidatorService;
 use Doctrine\ORM\EntityManagerInterface;
 
 class ZaakEigenschappenHandler implements ActionHandlerInterface
 {
     private EntityManagerInterface $entityManager;
+    private ObjectEntityService $objectEntityService;
+    private ValidatorService $validatorService;
 
     public function __construct(EntityManagerInterface $entityManager)
     {
         $this->entityManager = $entityManager;
     }
 
-
+    /**
+     * This function returns the identifierPath field from the configuration array
+     *
+     * @param array $data
+     * @param $configuration
+     * @return array
+     */
     public function getIdentifier(array $data, $configuration): string
     {
         $dotData = new \Adbar\Dot($data);
@@ -22,38 +32,125 @@ class ZaakEigenschappenHandler implements ActionHandlerInterface
         return $dotData->get($configuration['identifierPath']);
     }
 
-    public function createZaakEigenschap(?ObjectEntity $objectEntity): \App\Entity\Value
+    /**
+     * This function returns the eigenschappen field from the configuration array
+     *
+     * @param array $data
+     * @param $configuration
+     * @return array
+     */
+    public function getExtraElements(array $data, $configuration): array
     {
-        if ($objectEntity instanceof ObjectEntity) {
+        $dotData = new \Adbar\Dot($data);
 
-            $eigenschap = $objectEntity->getValueByAttribute($objectEntity->getEntity()->getAttributeByName('naam'));
-
-            var_dump($eigenschap);
-
-            // eigenschap matchen met een van de velden van extra elementen
-            // als we een match hebben pak url eigenschap
-            // maak zaakeigenschap aan met url van eigenschap en de waarde van extra element
-//            var_dump($objectEntity->toArray());
-        }
-
-        return $eigenschap;
+        return $dotData->get($configuration['eigenschappen']);
     }
 
+    /**
+     * This function adds the data to an object entity
+     *
+     * @param array $data
+     * @param ObjectEntity|null $objectEntity
+     * @return ObjectEntity
+     * @throws \App\Exception\GatewayException
+     * @throws \Psr\Cache\CacheException
+     * @throws \Psr\Cache\InvalidArgumentException
+     * @throws \Respect\Validation\Exceptions\ComponentException
+     */
+    private function populateObject(array $data, ObjectEntity $objectEntity): ObjectEntity
+    {
+        $owner = $this->objectEntityService->checkAndUnsetOwner($data);
+        if ($this->validatorService->validateData($data, $objectEntity->getEntity(), 'POST')) {
+            die;
+        }
+        $data = $this->objectEntityService->createOrUpdateCase($data, $objectEntity, $owner, 'POST', 'application/ld+json');
+
+        return $objectEntity;
+    }
+
+    /**
+     * This function creates a zaak eigenschap
+     *
+     * @param array $eigenschap
+     * @param ObjectEntity|null $objectEntity
+     * @return ObjectEntity
+     * @throws \App\Exception\GatewayException
+     * @throws \Psr\Cache\CacheException
+     * @throws \Psr\Cache\InvalidArgumentException
+     * @throws \Respect\Validation\Exceptions\ComponentException
+     */
+    public function createZaakEigenschap(array $eigenschap, ObjectEntity $objectEntity): ObjectEntity
+    {
+        $object = new ObjectEntity();
+        $object->setEntity($objectEntity->getEntity());
+        return $this->populateObject($eigenschap, $object);
+    }
+
+    /**
+     * This function returns the url and value of the eigenschap
+     *
+     * @param ObjectEntity|null $objectEntity
+     * @param array $extraElements
+     * @param string $eigenschap
+     * @param string $zaakUrl
+     * @return array|null
+     */
+    public function getEigenschapValues(ObjectEntity $objectEntity, array $extraElements, string $eigenschap, string $zaakUrl): ?array
+    {
+        foreach ($extraElements["ns1:extraElement"] as $element) {
+            if ($eigenschap == $element['@naam']) {
+                return [
+                    'zaak' => $zaakUrl,
+                    'eigenschap' => $objectEntity->getValueByAttribute($objectEntity->getEntity()->getAttributeByName('url'))->getStringValue(),
+                    'waarde' => $element['#']
+                ];
+            }
+        }
+        return null;
+    }
+
+    /**
+     * This function returns the name of the eigenschap
+     *
+     * @param ObjectEntity|null $objectEntity
+     * @param array $extraElements
+     * @param string $zaakUrl
+     * @return array|null
+     */
+    public function getEigenschap(?ObjectEntity $objectEntity, array $extraElements, string $zaakUrl): ?array
+    {
+        if ($objectEntity instanceof ObjectEntity) {
+            $eigenschap = $objectEntity->getValueByAttribute($objectEntity->getEntity()->getAttributeByName('naam'));
+
+            return $this->getEigenschapValues($objectEntity, $extraElements, $eigenschap->getStringValue(), $zaakUrl);
+        }
+        return null;
+    }
+
+    /**
+     * This function runs the zaakeigenschappen plugin
+     *
+     * @param array $data
+     * @param array $configuration
+     * @return array|null
+     * @throws \App\Exception\GatewayException
+     * @throws \Psr\Cache\CacheException
+     * @throws \Psr\Cache\InvalidArgumentException
+     * @throws \Respect\Validation\Exceptions\ComponentException
+     */
     public function __run(array $data, array $configuration): array
     {
         $identifier = $this->getIdentifier($data['request'], $configuration);
         $entity = $this->entityManager->getRepository('App:Entity')->findOneBy(['name' => 'Eigenschap']);
         $objectEntities = $this->entityManager->getRepository('App:ObjectEntity')->findByEntity($entity, ['zaaktype' => $identifier]);
 
-        var_dump($entity->getId()->toString());
-        var_dump(count($objectEntities));
         if (count($objectEntities) > 0) {
-            // get extra element velden naam en kijk of die overeenkomt met de naam van de eigenschap
             foreach ($objectEntities as $objectEntity) {
-                $objectEntity = $this->createZaakEigenschap($objectEntity);
+                $extraElements = $this->getExtraElements($data['request'], $configuration);
+                $eigenschap = $this->getEigenschap($objectEntity, $extraElements, $data['response']['url']);
+                $eigenschap !== null && $this->createZaakEigenschap($eigenschap, $objectEntity);
             }
         }
-        var_dump($identifier);
 
         return $data;
     }
