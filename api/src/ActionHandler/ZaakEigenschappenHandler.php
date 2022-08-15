d71af39986c5efdf46f4c7a2c8fd0d5b70ede474
Sarai Misidjan <Sarai@conduction.nl>
2022-08-15 14:50:02 +0200

    update zaak with unused extra elements

--- api/src/ActionHandler/ZaakEigenschappenHandler.php
+++ api/src/ActionHandler/ZaakEigenschappenHandler.php
@@ -12,6 +12,7 @@ class ZaakEigenschappenHandler implements ActionHandlerInterface
     private EntityManagerInterface $entityManager;
     private ObjectEntityService $objectEntityService;
     private ValidatorService $validatorService;
+    private array $usedValues = [];
 
     public function __construct(EntityManagerInterface $entityManager)
     {
@@ -53,6 +54,7 @@ class ZaakEigenschappenHandler implements ActionHandlerInterface
      *
      * @param array             $data The data from the call
      * @param ObjectEntity|null $objectEntity The object entity that relates to the entity Eigenschap
+     * @param string            $method The method of the call, defaulted to 'POST'
      *
      * @throws \App\Exception\GatewayException
      * @throws \Psr\Cache\CacheException
@@ -61,13 +63,13 @@ class ZaakEigenschappenHandler implements ActionHandlerInterface
      *
      * @return ObjectEntity
      */
-    private function populateObject(array $data, ObjectEntity $objectEntity): ObjectEntity
+    private function populateObject(array $data, ObjectEntity $objectEntity, string $method = 'POST'): ObjectEntity
     {
         $owner = $this->objectEntityService->checkAndUnsetOwner($data);
-        if ($this->validatorService->validateData($data, $objectEntity->getEntity(), 'POST')) {
+        if ($errors = $this->validatorService->validateData($data, $objectEntity->getEntity(), $method)) {
             exit;
         }
-        $data = $this->objectEntityService->createOrUpdateCase($data, $objectEntity, $owner, 'POST', 'application/ld+json');
+        $data = $this->objectEntityService->createOrUpdateCase($data, $objectEntity, $owner, $method, 'application/ld+json');
 
         return $objectEntity;
     }
@@ -77,6 +79,7 @@ class ZaakEigenschappenHandler implements ActionHandlerInterface
      *
      * @param array             $eigenschap The eigenschap array with zaak, eigenschap and waarde as keys
      * @param ObjectEntity|null $objectEntity The object entity that relates to the entity Eigenschap
+     * @param string            $method The method of the call, defaulted to 'POST'
      *
      * @throws \App\Exception\GatewayException
      * @throws \Psr\Cache\CacheException
@@ -85,12 +88,12 @@ class ZaakEigenschappenHandler implements ActionHandlerInterface
      *
      * @return ObjectEntity Creates a zaakeigenschap
      */
-    public function createZaakEigenschap(array $eigenschap, ObjectEntity $objectEntity): ObjectEntity
+    public function createOrUpdateObject(array $eigenschap, ObjectEntity $objectEntity, string $method = 'POST'): ObjectEntity
     {
         $object = new ObjectEntity();
         $object->setEntity($objectEntity->getEntity());
 
-        return $this->populateObject($eigenschap, $object);
+        return $this->populateObject($eigenschap, $object, $method);
     }
 
     /**
@@ -107,15 +110,47 @@ class ZaakEigenschappenHandler implements ActionHandlerInterface
     {
         foreach ($extraElements['ns1:extraElement'] as $element) {
             if ($eigenschap == $element['@naam']) {
+                $this->usedValues[] = $element['@naam'];
                 return [
                     'zaak'       => $zaakUrl,
                     'eigenschap' => $objectEntity->getValueByAttribute($objectEntity->getEntity()->getAttributeByName('url'))->getStringValue(),
                     'waarde'     => $element['#'],
+                    'zaaktype'   => $objectEntity->getValueByAttribute($objectEntity->getEntity()->getAttributeByName('zaaktype'))->getStringValue(),
+                    'definitie'  => $objectEntity->getValueByAttribute($objectEntity->getEntity()->getAttributeByName('definitie'))->getStringValue(),
+                    'naam'       => $objectEntity->getValueByAttribute($objectEntity->getEntity()->getAttributeByName('naam'))->getStringValue()
                 ];
             }
         }
 
         return null;
+    }
+
+    /**
+     * This function returns updates the zaak with the unused elements under 'toelichting'
+     *
+     * @param array $extraElements The extra elements that are taken from the action configuration eigenschappen path
+     * @param array $data The data from the call
+     *
+     * @return array|null
+     */
+    public function updateZaak(array $extraElements, array $data): ?array
+    {
+        $unusedElements = [
+            'toelichting' => '',
+            'zaaktype' => $data['zaaktype'],
+            'startdatum' => $data['startdatum'],
+            'bronorganisatie' => $data['bronorganisatie'],
+            'verantwoordelijkeOrganisatie' => $data['verantwoordelijkeOrganisatie']
+        ];
+
+        foreach ($extraElements['ns1:extraElement'] as $element) {
+            if (in_array($element['@naam'], $this->usedValues)) {
+                continue;
+            }
+            $unusedElements['toelichting'] .= "{$element['@naam']}: {$element['#']}";
+        }
+
+        return $unusedElements;
     }
 
     /**
@@ -156,14 +191,19 @@ class ZaakEigenschappenHandler implements ActionHandlerInterface
         $identifier = $this->getIdentifier($data['request'], $configuration);
         $entity = $this->entityManager->getRepository('App:Entity')->findOneBy(['name' => 'Eigenschap']);
         $objectEntities = $this->entityManager->getRepository('App:ObjectEntity')->findByEntity($entity, ['zaaktype' => $identifier]);
+        $extraElements = $this->getExtraElements($data['request'], $configuration);
 
         if (count($objectEntities) > 0) {
             foreach ($objectEntities as $objectEntity) {
-                $extraElements = $this->getExtraElements($data['request'], $configuration);
                 $eigenschap = $this->getEigenschap($objectEntity, $extraElements, $data['response']['url']);
-                $eigenschap !== null && $this->createZaakEigenschap($eigenschap, $objectEntity);
+                $eigenschap !== null && $this->createOrUpdateObject($eigenschap, $objectEntity, 'POST');
             }
         }
+
+        $zaak = $this->entityManager->getRepository('App:Entity')->findOneBy(['name' => 'Zaak']);
+        $zaakObject = $this->entityManager->getRepository('App:ObjectEntity')->findByEntity($zaak, ['url' => $data['response']['url']]);
+        $unusedElements = $this->updateZaak($extraElements, $data['response']);
+        $this->createOrUpdateObject($unusedElements, $zaakObject[0], 'PUT');
 
         return $data;
     }
