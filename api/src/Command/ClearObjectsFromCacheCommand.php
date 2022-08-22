f3c0753553b95422b477af89c444d0abec66aa90
Wilco Louwerse <wilco@conduction.nl>
2022-08-22 11:33:43 +0200

    Split clearObjectsCommand execute into multiple functions

--- api/src/Command/ClearObjectsFromCacheCommand.php
+++ api/src/Command/ClearObjectsFromCacheCommand.php
@@ -12,6 +12,8 @@ use Doctrine\ORM\EntityManagerInterface;
 use Ramsey\Uuid\Uuid;
 use Symfony\Component\Cache\Adapter\AdapterInterface as CacheInterface;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\HelperInterface;
+use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\ChoiceQuestion;
@@ -21,6 +23,8 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 
 class ClearObjectsFromCacheCommand extends Command
 {
+    private InputInterface $input;
+    private OutputInterface $output;
     private CacheInterface $cache;
     private FunctionService $functionService;
     private EntityManagerInterface $entityManager;
@@ -50,81 +54,92 @@ class ClearObjectsFromCacheCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        // todo: cleanup and split into more functions?
+        $this->input = $input;
+        $this->output = $output;
 
         $helper = $this->getHelper('question');
-        $question = new ChoiceQuestion(
-            'What type of entity are you going to give id\'s for? (Object, Entity, Collection or AllObjects. Default = Object. AllObjects will remove all objects from cache)',
-            ['Object', 'Entity', 'Collection', 'AllObjects'],
-            '0'
-        );
-        $question->setErrorMessage('Type %s is invalid.');
-        $type = $helper->ask($input, $output, $question);
-        $output->writeln('You have just selected: '.$type);
+        $type = $this->askForType($helper);
+        $io = new SymfonyStyle($input, $output);
 
         if ($type !== 'AllObjects') {
-            $question = new Question('Now please give one or more uuids for Type '.$type.' (Use Enter to start adding one more id and Ctrl+D to stop adding more)', 'NO UUID INPUT');
-            $question->setMultiline(true);
-            $ids = $helper->ask($input, $output, $question);
-            $ids = explode(PHP_EOL, $ids);
+            $ids = $this->getIdsAndStartProgress($io, $helper, $type);
             $total = count($ids);
-
-            $io = new SymfonyStyle($input, $output);
-            $io->title('Clear Objects from cache');
-            $io->section('Removing Objects from cache for Type: '.$type.' ('.$total.' id\'s given)');
-            $io->progressStart($total);
-            $io->newLine();
         }
 
         $this->functionService->removeResultFromCache = [];
         switch ($type) {
             case 'Object':
-                $errorCount = $this->handleSwitchType($io, $ids, 'ObjectEntity', 'Object from cache');
-//                $errorCount = $this->handleTypeObject($io, $ids);
+                $errorCount = $this->handleSwitchType($io, $ids ?? [], 'ObjectEntity', 'Object from cache');
                 break;
             case 'Entity':
-                $errorCount = $this->handleSwitchType($io, $ids, 'Entity', 'Objects from cache for Entity');
-//                $errorCount = $this->handleTypeEntity($io, $ids);
+                $errorCount = $this->handleSwitchType($io, $ids ?? [], 'Entity', 'Objects from cache for Entity');
                 break;
             case 'Collection':
-                $errorCount = $this->handleSwitchType($io, $ids, 'Entity', 'Objects from cache for Collection');
-//                $errorCount = $this->handleTypeCollection($io, $ids);
+                $errorCount = $this->handleSwitchType($io, $ids ?? [], 'CollectionEntity', 'Objects from cache for Collection');
                 break;
             case 'AllObjects':
-                $question = new ConfirmationQuestion('Are you sure you want to remove all objects from cache? (y/n) (default = n)', false);
-
-                if (!$helper->ask($input, $output, $question)) {
+                $objectEntities = $this->getAllObjectsAndStartProgress($io, $helper);
+                if ($objectEntities === null) {
                     return Command::SUCCESS;
                 }
-                $objectEntities = $this->entityManager->getRepository('App:ObjectEntity')->findAll();
                 $total = count($objectEntities);
 
-                $io = new SymfonyStyle($input, $output);
-                $io->title('Clear Objects from cache');
-                $io->section('Removing all Objects from cache ('.$total.' objects found)');
-                $io->progressStart($total);
-                $io->newLine();
-
-                $errorCount = $this->handleAllObjects($io, $objectEntities);
+                $errorCount = $this->removeAllObjectsFromCache($io, $objectEntities);
                 break;
             default:
                 return Command::INVALID;
         }
         $io->progressFinish();
 
-        $errors = round($errorCount / $total * 100) == 0 && $errorCount > 0 ? 1 : round($errorCount / $total * 100);
-        $typeString = $type !== 'Object' && $type !== 'AllObjects' ? 'for Type'.$type : '';
-        if ($errors == 0) {
-            $io->success("All Objects$typeString have been removed from cache");
-        } elseif ($errors < 20) {
-            $io->warning("Some Objects$typeString could not be removed from cache. Failure rate (per $type) is $errors%");
-        } else {
-            $io->error("A lot of Objects $typeString could not be removed from cache. Failure rate (per $type) is $errors%");
+        return $this->handleExecuteResponse($io, $type, $errorCount, $total ?? 0);
+    }
 
-            return Command::FAILURE;
+    private function askForType(QuestionHelper $helper): string
+    {
+        $question = new ChoiceQuestion(
+            'What type of entity are you going to give id\'s for? (Object, Entity, Collection or AllObjects. Default = Object. AllObjects will remove all objects from cache)',
+            ['Object', 'Entity', 'Collection', 'AllObjects'],
+            '0'
+        );
+        $question->setErrorMessage('Type %s is invalid.');
+        $type = $helper->ask($this->input, $this->output, $question);
+        $this->output->writeln('You have just selected: '.$type);
+
+        return $type;
+    }
+
+    private function getIdsAndStartProgress(SymfonyStyle $io, QuestionHelper $helper, string $type): array
+    {
+        $question = new Question('Now please give one or more uuids for Type '.$type.' (Use Enter to start adding one more id and Ctrl+D to stop adding more)', 'NO UUID INPUT');
+        $question->setMultiline(true);
+        $ids = $helper->ask($this->input, $this->output, $question);
+        $ids = explode(PHP_EOL, $ids);
+        $total = count($ids);
+
+        $io->title('Clear Objects from cache');
+        $io->section('Removing Objects from cache for Type: '.$type.' ('.$total.' id\'s given)');
+        $io->progressStart($total);
+        $io->newLine();
+
+        return $ids;
+    }
+
+    private function getAllObjectsAndStartProgress(SymfonyStyle $io, QuestionHelper $helper): ?array
+    {
+        $question = new ConfirmationQuestion('Are you sure you want to remove all objects from cache? (y/n) (default = n)', false);
+
+        if (!$helper->ask($this->input, $this->output, $question)) {
+            return null;
         }
+        $objectEntities = $this->entityManager->getRepository('App:ObjectEntity')->findAll();
+        $total = count($objectEntities);
 
-        return Command::SUCCESS;
+        $io->title('Clear Objects from cache');
+        $io->section('Removing all Objects from cache ('.$total.' objects found)');
+        $io->progressStart($total);
+        $io->newLine();
+
+        return $objectEntities;
     }
 
     private function handleSwitchType(SymfonyStyle $io, array $ids, string $entityName, string $ioSectionText): int
@@ -132,7 +147,7 @@ class ClearObjectsFromCacheCommand extends Command
         $errorCount = 0;
         foreach ($ids as $id) {
             $io->newLine();
-            $io->section("Removing {$ioSectionText} with id: {$id}"); //todo make this abstract
+            $io->section("Removing {$ioSectionText} with id: {$id}");
             if (!Uuid::isValid($id)) {
                 $io->error($id. ' is not a valid uuid!');
                 $errorCount++;
@@ -140,43 +155,14 @@ class ClearObjectsFromCacheCommand extends Command
                 continue;
             }
             $object = $this->entityManager->getRepository('App:'.$entityName)->find($id);
-            switch (get_class($object)) {
-                case ObjectEntity::class:
-                    $this->removeObjectFromCache($io, $object, $id);
-                    break;
-                case Entity::class:
-                    $this->removeEntityObjectsFromCache($io, $object, $id);
-                    break;
-                case CollectionEntity::class:
-                    $this->removeCollectionObjectsFromCache($io, $object, $id);
-                    break;
-                default:
-                    $io->error("Could not find an {$entityName} with this id: {$id}");
-                    $errorCount++;
-                    break;
-            }
-            $io->progressAdvance();
-
-            return false;
-        }
-
-        return true;
-    }
-
-    private function handleTypeObject(SymfonyStyle $io, array $ids): int
-    {
-        $errorCount = 0;
-        foreach ($ids as $id) {
-            $io->text('');
-            $io->section("Removing Object from cache with id: {$id}");
-            if (!$this->checkUuid($io, $id, $errorCount)) {
-                continue;
-            }
-            $objectEntity = $this->entityManager->getRepository('App:ObjectEntity')->find($id);
-            if ($objectEntity instanceof ObjectEntity) {
-                $this->removeObjectFromCache($io, $objectEntity, $id);
+            if ($object instanceof ObjectEntity) {
+                $this->removeObjectFromCache($io, $object, $id);
+            } elseif ($object instanceof Entity) {
+                $this->removeEntityObjectsFromCache($io, $object, $id);
+            } elseif ($object instanceof CollectionEntity) {
+                $this->removeCollectionObjectsFromCache($io, $object, $id);
             } else {
-                $io->error('Could not find an ObjectEntity with this id: '.$id);
+                $io->error("Could not find an {$entityName} with this id: {$id}");
                 $errorCount++;
             }
             $io->progressAdvance();
@@ -188,29 +174,7 @@ class ClearObjectsFromCacheCommand extends Command
     private function removeObjectFromCache(SymfonyStyle $io, ObjectEntity $objectEntity, string $id)
     {
         $this->functionService->removeResultFromCache($objectEntity);
-        $io->text("Successfully removed Object with id: {$id} from cache");
-    }
-
-    private function handleTypeEntity(SymfonyStyle $io, array $ids): int
-    {
-        $errorCount = 0;
-        foreach ($ids as $id) {
-            $io->text('');
-            $io->section("Removing Objects from cache for Entity with id: {$id}");
-            if (!$this->checkUuid($io, $id, $errorCount)) {
-                continue;
-            }
-            $entity = $this->entityManager->getRepository('App:Entity')->find($id);
-            if ($entity instanceof Entity) {
-                $this->removeEntityObjectsFromCache($io, $entity, $id);
-            } else {
-                $io->error('Could not find an Entity with this id: '.$id);
-                $errorCount++;
-            }
-            $io->progressAdvance();
-        }
-
-        return $errorCount;
+        $io->text("Successfully removed Object with id: {$id} (of Entity type: {$objectEntity->getEntity()->getName()}) from cache");
     }
 
     private function removeEntityObjectsFromCache(SymfonyStyle $io, Entity $entity, string $id)
@@ -221,29 +185,6 @@ class ClearObjectsFromCacheCommand extends Command
         $io->text("Successfully removed all Objects from cache for the Entity with id: {$id} (and name: {$entity->getName()})");
     }
 
-    private function handleTypeCollection(SymfonyStyle $io, array $ids): int
-    {
-        $errorCount = 0;
-
-        foreach ($ids as $id) {
-            $io->text('');
-            $io->section("Removing Objects from cache for Collection with id: {$id}");
-            if (!$this->checkUuid($io, $id, $errorCount)) {
-                continue;
-            }
-            $collection = $this->entityManager->getRepository('App:CollectionEntity')->find($id);
-            if ($collection instanceof CollectionEntity) {
-                $this->removeCollectionObjectsFromCache($io, $collection, $id);
-            } else {
-                $io->error('Could not find a Collection with this id: '.$id);
-                $errorCount++;
-            }
-            $io->progressAdvance();
-        }
-
-        return $errorCount;
-    }
-
     private function removeCollectionObjectsFromCache(SymfonyStyle $io, CollectionEntity $collection, string $id)
     {
         foreach ($collection->getEntities() as $entity) {
@@ -252,7 +193,7 @@ class ClearObjectsFromCacheCommand extends Command
         $io->text("Successfully removed all Objects from cache for the Collection with id: {$id} (and name: {$collection->getName()})");
     }
 
-    private function handleAllObjects(SymfonyStyle $io, array $objectEntities): int
+    private function removeAllObjectsFromCache(SymfonyStyle $io, array $objectEntities): int
     {
         $io->newLine();
         foreach ($objectEntities as $objectEntity) {
@@ -264,5 +205,22 @@ class ClearObjectsFromCacheCommand extends Command
         }
 
         return 0;
+    }
+
+    private function handleExecuteResponse(SymfonyStyle $io, string $type, int $errorCount, int $total): int
+    {
+        $errors = round($errorCount / $total * 100) == 0 && $errorCount > 0 ? 1 : round($errorCount / $total * 100);
+        $typeString = $type !== 'Object' && $type !== 'AllObjects' ? 'for Type '.$type : '';
+        if ($errors == 0) {
+            $io->success("All Objects $typeString have been removed from cache");
+        } elseif ($errors < 20) {
+            $io->warning("Some Objects $typeString could not be removed from cache. Failure rate (per $type) is $errors%");
+        } else {
+            $io->error("A lot of Objects $typeString could not be removed from cache. Failure rate (per $type) is $errors%");
+
+            return Command::FAILURE;
+        }
+
+        return Command::SUCCESS;
     }
 }
