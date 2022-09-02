9de4b2e3b77d9eda5120fb34b6bd69c9218e1063
Wilco Louwerse <wilco@conduction.nl>
2022-09-02 16:25:44 +0200

    What to do about this one? @Ruben

--- api/src/Service/TranslationService.php
+++ api/src/Service/TranslationService.php
@@ -27,9 +27,10 @@ class TranslationService
             }
             $result[$newKey] = $value;
 
-            if ($value === [] && $newKey != 'results') {
-                unset($result[$newKey]);
-            }
+            // todo: With this if statement it is impossible to do the following put: "telefoonnummers": []
+//            if ($value === [] && $newKey != 'results') {
+//                unset($result[$newKey]);
+//            }
         }
 
         return $result;
