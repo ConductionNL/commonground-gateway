28c896560401f62a650e96b3401853b60763c041
Wilco Louwerse <wilco@conduction.nl>
2022-10-26 21:43:10 +0200

    todo

--- api/src/Service/CatalogiService.php
+++ api/src/Service/CatalogiService.php
@@ -29,6 +29,15 @@ class CatalogiService
         $this->data = $data;
         $this->configuration = $configuration;
 
+        //todo:
+        // Get all the Catalogi we know of
+        // Get the Catalogi of all the Catalogi we know of
+        // Check if these Catalogi know any Catalogi we don't know yet
+        // Add any unknown Catalogi so we know them as well
+        // Repeat for newly added Catalogi (recursion)
+
+        // todo: how do we ever remove a Catalogi? If the existing Catalogi keep adding the removed Catalogi?
+
         return $data;
     }
 }
