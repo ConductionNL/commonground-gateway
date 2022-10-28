c7b2b392b3ab850acaab31269ab000ca44d65aad
Sarai Misidjan <Sarai@conduction.nl>
2022-10-28 12:24:56 +0200

    added dashboardCard entity

---
+++ api/src/Entity/DashboardCard.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\DashboardCardRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource(
+ *  normalizationContext={"groups"={"read"}, "enable_max_depth"=true},
+ *  denormalizationContext={"groups"={"write"}, "enable_max_depth"=true},
+ *  itemOperations={
+ *     "get"={"path"="/admin/dashboardCards/{id}"},
+ *     "put"={"path"="/admin/dashboardCards/{id}"},
+ *     "delete"={"path"="/admin/dashboardCards/{id}"}
+ *  },
+ *  collectionOperations={
+ *     "get"={"path"="/admin/dashboardCards"},
+ *     "post"={"path"="/admin/dashboardCards"}
+ *  })
+ * @ORM\Entity(repositoryClass=DashboardCardRepository::class)
+ */
+class DashboardCard
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $type;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $entity;
+
+    /**
+     * @ORM\Column(type="uuid")
+     */
+    private $uuid;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $ordering;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getEntity(): ?string
+    {
+        return $this->entity;
+    }
+
+    public function setEntity(string $entity): self
+    {
+        $this->entity = $entity;
+
+        return $this;
+    }
+
+    public function getUuid()
+    {
+        return $this->uuid;
+    }
+
+    public function setUuid($uuid): self
+    {
+        $this->uuid = $uuid;
+
+        return $this;
+    }
+
+    public function getOrdering(): ?int
+    {
+        return $this->ordering;
+    }
+
+    public function setOrdering(int $ordering): self
+    {
+        $this->ordering = $ordering;
+
+        return $this;
+    }
+}
