2d1d1247b50e62d079f37ed2a7e31c563bbccba4
Robert Zondervan <robert@conduction.nl>
2022-11-03 15:20:38 +0100

    remove verblijfsadres

--- api/src/Service/ZdsZaakService.php
+++ api/src/Service/ZdsZaakService.php
@@ -266,7 +266,6 @@ class ZdsZaakService
             if ($natuurlijkPersoonObject = $heeftAlsInitiatorObject->getValue('natuurlijkPersoon')) {
                 $rol->setValue('betrokkeneIdentificatie', $natuurlijkPersoonObject->toArray());
                 $rol->setValue('betrokkeneType', 'natuurlijk_persoon');
-                $rol->getValue('betrokkeneIdentificatie')->setValue('verblijfsadres', $natuurlijkPersoonObject->getValue('verblijfadres')->toArray());
             }
 
             if ($heeftAlsInitiatorObject->getValue('vestiging')->getValue('vestigingsNummer') || $heeftAlsInitiatorObject->getValue('vestiging')->getValue('handelsnaam')) {
