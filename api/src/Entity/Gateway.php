4def3797392ce9ec54b6323f9377bda960786a81
Ruben van der Linde <ruben@conduction.nl>
2022-11-17 15:29:26 +0100

    Merge conflits

--- api/src/Entity/Gateway.php
+++ api/src/Entity/Gateway.php
@@ -667,16 +667,6 @@ class Gateway
      */
     private $dateModified;
 
-    /**
-     * @ORM\Column(type="datetime", nullable=true)
-     */
-    private $lastCall;
-
-    /**
-     * @ORM\Column(type="integer", nullable=true)
-     */
-    private $status;
-
     public function __construct()
     {
         $this->responceLogs = new ArrayCollection();
@@ -1219,30 +1209,6 @@ class Gateway
     public function setConfiguration(?array $configuration): self
     {
         $this->configuration = $configuration;
-
-        return $this;
-    }
-
-    public function getLastCall(): ?\DateTimeInterface
-    {
-        return $this->lastCall;
-    }
-
-    public function setLastCall(?\DateTimeInterface $lastCall): self
-    {
-        $this->lastCall = $lastCall;
-
-        return $this;
-    }
-
-    public function getStatus(): ?int
-    {
-        return $this->status;
-    }
-
-    public function setStatus(?int $status): self
-    {
-        $this->status = $status;
 
         return $this;
     }
