1ecb845d18c37ec545a60b97e19c12e97f37ea27
Robert Zondervan <robert@conduction.nl>
2022-11-22 10:18:03 +0100

    (very) temporarily disable filtering of documents

--- api/src/Service/ZdsZaakService.php
+++ api/src/Service/ZdsZaakService.php
@@ -567,7 +567,7 @@ class ZdsZaakService
         $document->setValue('beschrijving', $zdsObject->getValue('dctOmschrijving'));
         $document->setValue('informatieobjecttype', $informatieobjecttypenObjectEntity->getValue('url'));
         $document->setValue('vertrouwelijkheidaanduiding', $informatieobjecttypenObjectEntity->getValue('vertrouwelijkheidaanduiding'));
-        $document->setValue('indicatieGebruiksrecht', true);
+//        $document->setValue('indicatieGebruiksrecht', true);
         //        $document->setValue('bestandsnaam', $zdsObject->getValue(''));
         //        $document->setValue('ontvangstdatum', $zdsObject->getValue(''));
         //        $document->setValue('verzenddatum', $zdsObject->getValue('')); // stuurgegevens.tijdstipBericht
@@ -806,7 +806,7 @@ class ZdsZaakService
         $heeftRelevant = [];
         foreach ($documenten as $document) {
             $enkelvoudigInformatieObject = $document->getValue('informatieobject');
-            if (!$enkelvoudigInformatieObject || !$enkelvoudigInformatieObject->getValue('indicatieGebruiksrecht')) {
+            if (!$enkelvoudigInformatieObject /*|| !$enkelvoudigInformatieObject->getValue('indicatieGebruiksrecht')*/) {
                 continue;
             }
             $createDate = new DateTime($enkelvoudigInformatieObject->getValue('creatiedatum'));
