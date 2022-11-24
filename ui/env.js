08ef94c748fc346da48a6e9115f4bb8cdb684207
Ruben van der Linde <ruben@conduction.nl>
2022-11-24 10:03:02 +0100

    Add the new ui to the standard docker compose

---
+++ ui/env.js
@@ -0,0 +1,16 @@
+window.sessionStorage.setItem("GATSBY_ME_URL", "https://localhost/me");
+window.sessionStorage.setItem("GATSBY_API_URL", "https://localhost/api");
+window.sessionStorage.setItem(
+  "GATSBY_ADMIN_URL",
+  "https://localhost/admin"
+);
+window.sessionStorage.setItem("GATSBY_BASE_URL", "https://localhost/");
+window.sessionStorage.setItem(
+  "GATSBY_FRONTEND_URL",
+  "https://localhost:8000"
+);
+window.sessionStorage.setItem(
+  "GATSBY_ORGANIZATION",
+  "http://webresourcecatalogus.conduction.svc.cluster.local/organizations/b2d3176e-f1c6-4365-ab86-dd253c65fc43"
+);
+window.sessionStorage.setItem("GATSBY_LOGIN_REDIRECT", "vault");
