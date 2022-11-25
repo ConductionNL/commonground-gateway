490274f1f7b553cacc302b030925cf49c2710907
Barry Brands <barrybrands02@hotmail.com>
2022-11-25 17:50:08 +0100

    CallLog properties added

--- api/src/Entity/CallLog.php
+++ api/src/Entity/CallLog.php
@@ -81,6 +81,22 @@ class CallLog
     private string $method = '';
 
     /**
+     * @var string The body of the request
+     *
+     * @Groups({"read","read_secure"})
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private string $requestBody = '';
+
+    /**
+     * @var array The headers of the response
+     *
+     * @Groups({"read","read_secure"})
+     * @ORM\Column(type="array", nullable=true)
+     */
+    private array $requestHeaders = [];
+
+    /**
      * @var string the response status of the request
      *
      * @Groups({"read","read_secure"})
@@ -103,6 +119,14 @@ class CallLog
      * @ORM\Column(type="text", nullable=true)
      */
     private string $responseBody = '';
+
+    /**
+     * @var array The headers of the response
+     *
+     * @Groups({"read","read_secure"})
+     * @ORM\Column(type="array", nullable=true)
+     */
+    private array $responseHeaders = [];
 
     /**
      * @var int the runtime of the request
@@ -140,7 +164,7 @@ class CallLog
         $status = $this->source->getStatus();
         // If we only have a status code set code if we have code and status text set both
         isset($this->responseStatusCode) && !empty($this->responseStatusCode) && $status = $this->responseStatusCode;
-        isset($this->responseStatusCode, $this->responseStatus) && !empty($this->responseStatusCode) && !empty($this->responseStatus) && $status = $this->responseStatusCode.' '.$this->responseStatus;
+        isset($this->responseStatusCode, $this->responseStatus) && !empty($this->responseStatusCode) && !empty($this->responseStatus) && $status = $this->responseStatusCode . ' ' . $this->responseStatus;
         $this->source->setStatus($status);
     }
 
@@ -197,6 +221,30 @@ class CallLog
         return $this;
     }
 
+    public function getRequestBody(): ?string
+    {
+        return $this->requestBody;
+    }
+
+    public function setRequestBody(?string $requestBody): self
+    {
+        $this->requestBody = $requestBody;
+
+        return $this;
+    }
+
+    public function getRequestHeaders(): ?array
+    {
+        return $this->requestHeaders;
+    }
+
+    public function setRequestHeaders(?array $requestHeaders): self
+    {
+        $this->requestHeaders = $requestHeaders;
+
+        return $this;
+    }
+
     public function getResponseStatus(): ?string
     {
         return $this->responseStatus;
@@ -229,6 +277,18 @@ class CallLog
     public function setResponseBody(?string $responseBody): self
     {
         $this->responseBody = $responseBody;
+
+        return $this;
+    }
+
+    public function getResponseHeaders(): ?array
+    {
+        return $this->responseHeaders;
+    }
+
+    public function setResponseHeaders(?array $responseHeaders): self
+    {
+        $this->responseHeaders = $responseHeaders;
 
         return $this;
     }
