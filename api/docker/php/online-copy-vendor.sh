84b0e98cefed51a5e2967737ff5f2daeed680f54
Robert Zondervan <robert@conduction.nl>
2022-11-30 14:15:02 +0100

    Another syntax fix

--- api/docker/php/online-copy-vendor.sh
+++ api/docker/php/online-copy-vendor.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 set -e
 
-if [! -f "/tmp/vendor/composer.json" ]; then
+if [ ! -f "/tmp/vendor/composer.json" ]; then
 	cp /srv/api/composer.json /tmp/vendor/composer.json
 fi
-if [! -f "/tmp/vendor/composer.lock" ]; then
+if [ ! -f "/tmp/vendor/composer.lock" ]; then
 	cp /srv/api/composer.json /tmp/vendor/composer.lock
 fi
-if [! -d "/tmp/vendor/vendor" ]; then
+if [ ! -d "/tmp/vendor/vendor" ]; then
 	cp /srv/api/vendor /tmp/vendor/vendor -R
 fi
