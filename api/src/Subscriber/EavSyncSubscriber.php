8befe24932c51c79c8a2c103c208666d93dcfd91
Ruben van der Linde <ruben@conduction.nl>
2022-12-03 19:56:50 +0100

    Small fixes for plugin search endpoint

--- api/src/Subscriber/EavSyncSubscriber.php
+++ api/src/Subscriber/EavSyncSubscriber.php
@@ -53,10 +53,11 @@ class EavSyncSubscriber implements EventSubscriberInterface
         $source = $this->entityManager->getRepository('App:Gateway')->findOneBy(['id'=>$sourceId]);
 
 
-        $sourceId = $event->getRequest()->query->get('sourceId','');
+        $sourceId = $event->getRequest()->query->get('externalId','');
         $endpoint = $event->getRequest()->query->get('endpoint',null);
-        $actionId = $event->getRequest()->query->get('sourceId',null);
+        $actionId = $event->getRequest()->query->get('action',null);
         // Get a sync objcet
+
         $status = 202;
         if(!$synchronization = $this->entityManager->getRepository('App:Synchronization')->findOneBy(['object' => $objectEntity->getId(), 'gateway' => $source])){
             $synchronization = New Synchronization();
