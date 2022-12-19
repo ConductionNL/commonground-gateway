a14490f07d4036fffe6c7370f9314fd4bde195b7
Robert Zondervan <robert@conduction.nl>
2022-12-19 17:16:54 +0100

    Remove chmod from entrypoint

--- api/docker/php/docker-entrypoint.sh
+++ api/docker/php/docker-entrypoint.sh
@@ -15,7 +15,7 @@ if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 
 	mkdir -p var/cache var/log
 
-	chmod -R +rwx var
+#	chmod -R +rwx var
 
 #	setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX var
 #	setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX var
