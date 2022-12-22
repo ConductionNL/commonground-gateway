ae81e2a03a5501e8d7ae93b2e7eaa75ba16bcc89
Robert Zondervan <robert@conduction.nl>
2022-12-22 14:07:46 +0100

    rename schema column

--- api/src/Entity/Attribute.php
+++ api/src/Entity/Attribute.php
@@ -665,7 +665,7 @@ class Attribute
      * @var ?string The uri to a schema.org property
      *
      * @Groups({"read", "write"})
-     * @ORM\Column(type="string", length=255, nullable=true, options={"default":null})
+     * @ORM\Column(type="string", length=255, nullable=true, options={"default":null}, name="schema_column")
      */
     private ?string $schema = null;
 
