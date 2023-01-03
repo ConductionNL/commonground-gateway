4cd98b5a0e57671ea22b57423e049fb0bd008c78
Robert Zondervan <robert@conduction.nl>
2023-01-03 12:37:04 +0100

    Copy composer.lock to composer.lock online

--- api/docker/php/online-copy-vendor.sh
+++ api/docker/php/online-copy-vendor.sh
@@ -5,7 +5,7 @@ if [ ! -f "/tmp/vendor/composer.json" ]; then
 	cp /srv/api/composer.json /tmp/vendor/composer.json
 fi
 if [ ! -f "/tmp/vendor/composer.lock" ]; then
-	cp /srv/api/composer.json /tmp/vendor/composer.lock
+	cp /srv/api/composer.lock /tmp/vendor/composer.lock
 fi
 if [ ! -d "/tmp/vendor/vendor" ]; then
 	cp /srv/api/vendor /tmp/vendor/vendor -R
