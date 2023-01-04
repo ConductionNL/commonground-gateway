9c8999690219ee00ac28a8eeb0e76f7e7215e9b2
Robert Zondervan <robert@conduction.nl>
2023-01-04 15:05:18 +0100

    Long shot on copying files in init container

--- api/docker/php/online-copy-vendor.sh
+++ api/docker/php/online-copy-vendor.sh
@@ -4,15 +4,15 @@ ls -la /srv/api
 
 if [ ! -f "/tmp/vendor/composer.json" ]; then
 	echo "Copying composer.json"
-	cp /srv/api/composer.json /tmp/vendor/composer.json
+	cp /srv/api/composer.json /tmp/vendor/
 fi
 if [ ! -f "/tmp/vendor/composer.lock" ]; then
 	echo "Copying composer.lock"
-	cp /srv/api/composer.lock /tmp/vendor/composer.lock
+	cp /srv/api/composer.lock /tmp/vendor/
 fi
 if [ ! -f "/tmp/vendor/symfony.lock" ]; then
 	echo "Copying symfony.json"
-	cp /srv/api/symfony.lock /tmp/vendor/symfony.lock
+	cp /srv/api/symfony.lock /tmp/vendor/
 fi
 if [ ! -d "/tmp/vendor/vendor" ]; then
 	echo "Copying vendor folder"
