7e687eddeb92c8951af9a54cf20e10b84b3dcee1
Robert Zondervan <robert@conduction.nl>
2023-01-05 11:34:12 +0100

    Do not overwrite in init container

--- api/docker/php/online-copy-vendor.sh
+++ api/docker/php/online-copy-vendor.sh
@@ -4,19 +4,19 @@ ls -la /srv/api
 
 if [ ! -f "/tmp/vendor/composer.json" ]; then
 	echo "Copying composer.json"
-	cp /srv/api/composer.json /tmp/vendor/
+	cp /srv/api/composer.json /tmp/vendor/ -n
 fi
 if [ ! -f "/tmp/vendor/composer.lock" ]; then
 	echo "Copying composer.lock"
-	cp /srv/api/composer.lock /tmp/vendor/
+	cp /srv/api/composer.lock /tmp/vendor/ -n
 fi
 if [ ! -f "/tmp/vendor/symfony.lock" ]; then
 	echo "Copying symfony.json"
-	cp /srv/api/symfony.lock /tmp/vendor/
+	cp /srv/api/symfony.lock /tmp/vendor/ -n
 fi
 if [ ! -d "/tmp/vendor/vendor" ]; then
 	echo "Copying vendor folder"
-	cp /srv/api/vendor /tmp/vendor/vendor -R
+	cp /srv/api/vendor /tmp/vendor/vendor -Rn
 fi
 
 ls -la /tmp/vendor
