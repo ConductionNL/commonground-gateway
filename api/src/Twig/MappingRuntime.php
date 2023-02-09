bc96ba1c79e0cc1cae0ff8ff40f61faf8c6eed2d
Robert Zondervan <robert@conduction.nl>
2023-02-09 15:06:15 +0100

    Mapping functions for twig

---
+++ api/src/Twig/MappingRuntime.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Twig;
+
+use CommonGateway\CoreBundle\Service\MappingService;
+use Doctrine\ORM\EntityManagerInterface;
+use Faker\Factory;
+use Faker\Generator;
+use Twig\Extension\RuntimeExtensionInterface;
+
+class MappingRuntime implements RuntimeExtensionInterface
+{
+    private MappingService $mappingService;
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(MappingService $mappingService, EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+        $this->mappingService = $mappingService;
+    }
+
+    public function map(string $mappingString, array $data): array
+    {
+        $mapping = $this->entityManager->getRepository('App:Mapping')->findOneBy(['reference' => $mappingString]);
+        $value = $this->mappingService->mapping($mapping, $data);
+        return $value;
+    }
+}
