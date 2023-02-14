f20d4161b439ccc4da0f08654cfd427f01c170c6
Barry Brands <barrybrands02@hotmail.com>
2023-02-14 14:34:43 +0100

    bundle update

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -23,4 +23,5 @@ return [
     CommonGateway\ZGWBundle\ZGWBundle::class                                   => ['all' => true],
     CommonGateway\KlantenBundle\KlantenBundle::class                           => ['all' => true],
     CommonGateway\HuwelijksplannerBundle\HuwelijksplannerBundle::class         => ['all' => true],
+    CommonGateway\BRPBundle\BRPBundle::class                                   => ['all' => true],
 ];
