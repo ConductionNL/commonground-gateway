86e5b1f97eba2e96e360399a4614200efbc7a10c
Robert Zondervan <robert@conduction.nl>
2023-02-23 14:03:48 +0100

    Try installing bundle online

--- api/docker/php/online-copy-vendor.sh
+++ api/docker/php/online-copy-vendor.sh
@@ -3,16 +3,19 @@ set -e
 ls -la /srv/api
 
 if [ ! -f "/tmp/vendor/composer.json" ]; then
+	touch /tmp/vendor/composer.json
 	echo "Copying composer.json"
-	cp /srv/api/composer.json /tmp/vendor/ -n
+	cp /srv/api/composer.json /tmp/vendor/ -nf
 fi
 if [ ! -f "/tmp/vendor/composer.lock" ]; then
+	touch /tmp/vendor/composer.lock
 	echo "Copying composer.lock"
-	cp /srv/api/composer.lock /tmp/vendor/ -n
+	cp /srv/api/composer.lock /tmp/vendor/ -nf
 fi
 if [ ! -f "/tmp/vendor/symfony.lock" ]; then
-	echo "Copying symfony.json"
-	cp /srv/api/symfony.lock /tmp/vendor/ -n
+	touch /tmp/vendor/symfony.lock
+	echo "Copying symfony.lock"
+	cp /srv/api/symfony.lock /tmp/vendor/ -nf
 fi
 if [ ! -d "/tmp/vendor/vendor" ]; then
 	echo "Copying vendor folder"
