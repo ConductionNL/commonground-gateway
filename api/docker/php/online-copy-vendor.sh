568c271ab56e7f9e727b61f03884d428c366195a
Robert Zondervan <robert@conduction.nl>
2023-02-23 14:47:45 +0100

    do overwrite

--- api/docker/php/online-copy-vendor.sh
+++ api/docker/php/online-copy-vendor.sh
@@ -5,21 +5,21 @@ ls -la /srv/api
 if [ ! -f "/tmp/vendor/composer.json" ]; then
 	touch /tmp/vendor/composer.json
 	echo "Copying composer.json"
-	cp /srv/api/composer.json /tmp/vendor/ -nf
+	cp /srv/api/composer.json /tmp/vendor/ -f
 fi
 if [ ! -f "/tmp/vendor/composer.lock" ]; then
 	touch /tmp/vendor/composer.lock
 	echo "Copying composer.lock"
-	cp /srv/api/composer.lock /tmp/vendor/ -nf
+	cp /srv/api/composer.lock /tmp/vendor/ -f
 fi
 if [ ! -f "/tmp/vendor/symfony.lock" ]; then
 	touch /tmp/vendor/symfony.lock
 	echo "Copying symfony.lock"
-	cp /srv/api/symfony.lock /tmp/vendor/ -nf
+	cp /srv/api/symfony.lock /tmp/vendor/ -f
 fi
 if [ ! -d "/tmp/vendor/vendor" ]; then
 	echo "Copying vendor folder"
-	cp /srv/api/vendor /tmp/vendor/vendor -Rn
+	cp /srv/api/vendor /tmp/vendor/vendor -R
 fi
 
 ls -la /tmp/vendor
