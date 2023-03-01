97858b40276e4388035fe92e6f20e4222e69493f
Robert Zondervan <robert@conduction.nl>
2023-03-01 12:56:29 +0100

    remove touches

--- api/docker/php/online-copy-vendor.sh
+++ api/docker/php/online-copy-vendor.sh
@@ -3,22 +3,18 @@ set -e
 ls -la /srv/api
 
 if [ ! -f "/tmp/vendor/composer.json" ]; then
-	touch /tmp/vendor/composer.json
 	echo "Copying composer.json"
 	cp /srv/api/composer.json /tmp/vendor/ -f
 fi
 if [ ! -f "/tmp/vendor/composer.lock" ]; then
-	touch /tmp/vendor/composer.lock
 	echo "Copying composer.lock"
 	cp /srv/api/composer.lock /tmp/vendor/ -f
 fi
 if [ ! -f "/tmp/vendor/symfony.lock" ]; then
-	touch /tmp/vendor/symfony.lock
 	echo "Copying symfony.lock"
 	cp /srv/api/symfony.lock /tmp/vendor/ -f
 fi
 if [ ! -f "/tmp/vendor/bundles.php" ]; then
-	touch /tmp/vendor/bundles.php
 	echo "Copying bundles.php"
 	cp /srv/api/config/bundles.php /tmp/vendor/ -f
 fi
