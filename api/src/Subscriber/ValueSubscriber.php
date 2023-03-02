2f448a37e4f801d4b32a82a0c6ba63ca80aaa2a2
Robert Zondervan <robert@conduction.nl>
2023-03-02 17:39:31 +0100

    Add subobjects via url

--- api/src/Subscriber/ValueSubscriber.php
+++ api/src/Subscriber/ValueSubscriber.php
@@ -2,8 +2,12 @@
 
 namespace App\Subscriber;
 
+use App\Entity\Entity;
+use App\Entity\Gateway;
 use App\Entity\ObjectEntity;
+use App\Entity\Synchronization;
 use App\Entity\Value;
+use App\Service\SynchronizationService;
 use Doctrine\Bundle\DoctrineBundle\EventSubscriber\EventSubscriberInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Events;
@@ -11,6 +15,7 @@ use Doctrine\ORM\NonUniqueResultException;
 use Doctrine\Persistence\Event\LifecycleEventArgs;
 use Psr\Log\LoggerInterface;
 use Ramsey\Uuid\Uuid;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 class ValueSubscriber implements EventSubscriberInterface
 {
@@ -24,10 +29,22 @@ class ValueSubscriber implements EventSubscriberInterface
      */
     private LoggerInterface $logger;
 
-    public function __construct(EntityManagerInterface $entityManager, LoggerInterface $valueSubscriberLogger)
+    /**
+     * @var SynchronizationService $synchronizationService
+     */
+    private SynchronizationService $synchronizationService;
+
+    /**
+     * @var ParameterBagInterface
+     */
+    private ParameterBagInterface $parameterBag;
+
+    public function __construct(EntityManagerInterface $entityManager, LoggerInterface $valueSubscriberLogger, SynchronizationService $synchronizationService, ParameterBagInterface $parameterBag)
     {
         $this->entityManager = $entityManager;
         $this->logger = $valueSubscriberLogger;
+        $this->synchronizationService = $synchronizationService;
+        $this->parameterBag = $parameterBag;
     }
 
     public function getSubscribedEvents(): array
@@ -39,7 +56,7 @@ class ValueSubscriber implements EventSubscriberInterface
         ];
     }
 
-    public function getSubObject(string $uuid, Value $valueObject): ?ObjectEntity
+    public function getSubObjectById(string $uuid, Value $valueObject): ?ObjectEntity
     {
         $parentObject = $valueObject->getObjectEntity();
         if (!$subObject = $this->entityManager->find(ObjectEntity::class, $uuid)) {
@@ -74,20 +91,87 @@ class ValueSubscriber implements EventSubscriberInterface
         return $subObject;
     }
 
+    public function aquireObject(string $url, Entity $entity): ?ObjectEntity
+    {
+        // 1. Get the domain from the url
+        $parse = parse_url($url);
+        $location = $parse['host'];
+
+        // 2.c Try to establich a source for the domain
+        $source = $this->entityManager->getRepository('App:Gateway')->findOneBy(['location'=>$location]);
+
+        // 2.b The source might be on a path e.g. /v1 so if whe cant find a source let try to cycle
+        foreach(explode('/', $parse['path']) as $pathPart) {
+            $location = $location.'/'.$pathPart;
+            $source = $this->entityManager->getRepository('App:Gateway')->findOneBy(['location'=>$location]);
+            if($source !== null) {
+                break;
+            }
+        }
+        if($source instanceof Gateway === false) {
+            return null;
+        }
+
+        // 3 If we have a source we can establich an endpoint.
+        $endpoint = str_replace($location,'',$url);
+
+        // 4 Createa sync
+        $synchronization = new Synchronization($source, $entity);
+        $synchronization->setSourceId($url);
+        $synchronization->setEndpoint($endpoint);
+
+        $this->synchronizationService->synchronize($synchronization);
+
+        return $synchronization->getObject();
+    }
+
+    public function getSubObjectByUrl(string $url, Value $valueObject): ?ObjectEntity
+    {
+        $self = str_replace($this->parameterBag->get('app_url'), '', $url);
+        $objecEntity = $this->entityManager->getRepository('App:ObjectEntity')->findOneBy(['self' => $self]);
+        if($objecEntity !== null) {
+            return $objecEntity;
+        }
+
+        $synchronization = $this->entityManager->getRepository('App:Synchronization')->findOneBy(['sourceId' => $url]);
+        if($synchronization instanceof Synchronization === true) {
+            return $synchronization->getObject();
+        } else {
+            $this->aquireObject($url, $valueObject->getAttribute()->getObject());
+        }
+
+        return null;
+    }
+
+
+    public function findSubobject(string $identifier, Value $valueObject): ?ObjectEntity
+    {
+
+        if(Uuid::isValid($identifier)) {
+            $subObject = $this->getSubObjectById($identifier, $valueObject);
+            $subObject && $valueObject->addObject($subObject);
+        } elseif (filter_var($identifier, FILTER_VALIDATE_URL)) {
+            $subObject = $this->getSubObjectByUrl($identifier, $valueObject);
+        }
+
+        return $subObject;
+    }
+
+
     public function preUpdate(LifecycleEventArgs $value): void
     {
         $valueObject = $value->getObject();
 
         if ($valueObject instanceof Value && $valueObject->getAttribute()->getType() == 'object') {
             if ($valueObject->getArrayValue()) {
-                foreach ($valueObject->getArrayValue() as $uuid) {
-                    $subObject = $this->getSubObject($uuid, $valueObject);
-                    $subObject && $valueObject->addObject($subObject);
+                foreach ($valueObject->getArrayValue() as $identifier) {
+                    $valueObject->addObject($this->findSubobject($identifier, $valueObject));
                 }
                 $valueObject->setArrayValue([]);
-            } elseif (($uuid = $valueObject->getStringValue()) && Uuid::isValid($valueObject->getStringValue())) {
-                $subObject = $this->getSubObject($uuid, $valueObject);
-                $subObject && $valueObject->addObject($subObject);
+            } elseif ($identifier = $valueObject->getStringValue()) {
+                foreach($valueObject->getObjects() as $object)
+                    $valueObject->removeObject($object);
+                $valueObject->addObject($this->findSubobject($identifier, $valueObject));
             }
         }
     }
