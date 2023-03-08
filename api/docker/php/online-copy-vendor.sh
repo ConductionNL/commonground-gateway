78eab7095bba4108fa6ffeb676a72dd3afd09c54
Robert Zondervan <robert@conduction.nl>
2023-03-08 20:47:42 +0100

    copy with options on the left, ls of vendor folder

--- api/docker/php/online-copy-vendor.sh
+++ api/docker/php/online-copy-vendor.sh
@@ -1,26 +1,27 @@
 #!/bin/sh
 set -e
 ls -la /srv/api
+ls -la /tmp/vendor
 
 if [ ! -f "/tmp/vendor/composer.json" ]; then
 	echo "Copying composer.json"
-	cp /srv/api/composer.json /tmp/vendor/ -f
+	cp -f /srv/api/composer.json /tmp/vendor/composer.json
 fi
 if [ ! -f "/tmp/vendor/composer.lock" ]; then
 	echo "Copying composer.lock"
-	cp /srv/api/composer.lock /tmp/vendor/ -f
+	cp -f /srv/api/composer.lock /tmp/vendor/composer.lock
 fi
 if [ ! -f "/tmp/vendor/symfony.lock" ]; then
 	echo "Copying symfony.lock"
-	cp /srv/api/symfony.lock /tmp/vendor/ -f
+	cp -f /srv/api/symfony.lock /tmp/vendor/symfony.lock
 fi
 if [ ! -f "/tmp/vendor/bundles.php" ]; then
 	echo "Copying bundles.php"
-	cp /srv/api/config/bundles.php /tmp/vendor/ -f
+	cp -f /srv/api/config/bundles.php /tmp/vendor/bundles.php
 fi
 if [ ! -d "/tmp/vendor/vendor" ]; then
 	echo "Copying vendor folder"
-	cp /srv/api/vendor /tmp/vendor/vendor -R
+	cp -R /srv/api/vendor /tmp/vendor/vendor
 fi
 
 ls -la /tmp/vendor
