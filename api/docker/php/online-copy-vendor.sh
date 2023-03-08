f06ddcecb5f1cb2b59795e819d795ab9453e521a
Robert Zondervan <robert@conduction.nl>
2023-03-08 21:05:53 +0100

    write files to folder

--- api/docker/php/online-copy-vendor.sh
+++ api/docker/php/online-copy-vendor.sh
@@ -5,19 +5,19 @@ ls -la /tmp/vendor
 
 if [ ! -f "/tmp/vendor/composer.json" ]; then
 	echo "Copying composer.json"
-	cp -f /srv/api/composer.json /tmp/vendor/composer.json
+	cp -f /srv/api/composer.json /tmp/vendor/
 fi
 if [ ! -f "/tmp/vendor/composer.lock" ]; then
 	echo "Copying composer.lock"
-	cp -f /srv/api/composer.lock /tmp/vendor/composer.lock
+	cp -f /srv/api/composer.lock /tmp/vendor/
 fi
 if [ ! -f "/tmp/vendor/symfony.lock" ]; then
 	echo "Copying symfony.lock"
-	cp -f /srv/api/symfony.lock /tmp/vendor/symfony.lock
+	cp -f /srv/api/symfony.lock /tmp/vendor/
 fi
 if [ ! -f "/tmp/vendor/bundles.php" ]; then
 	echo "Copying bundles.php"
-	cp -f /srv/api/config/bundles.php /tmp/vendor/bundles.php
+	cp -f /srv/api/config/bundles.php /tmp/vendor/
 fi
 if [ ! -d "/tmp/vendor/vendor" ]; then
 	echo "Copying vendor folder"
