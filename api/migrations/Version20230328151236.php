0b6b141f133d97b0f0e5c6244658c98a3ad905e8
Wilco Louwerse <wilco@conduction.nl>
2023-03-28 19:39:55 +0200

    Lets make codacy happy

--- api/migrations/Version20230328151236.php
+++ api/migrations/Version20230328151236.php
@@ -12,14 +12,30 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20230328151236 extends AbstractMigration
 {
+
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
     public function getDescription(): string
     {
         return '';
-    }
 
+    }//end getDescription()
+
+
+    /**
+     * Up
+     *
+     * @param Schema $schema Schema
+     *
+     * @return void
+     */
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
+        // This up() migration is auto-generated, please modify it to your needs.
         $this->addSql('ALTER TABLE security_group ADD reference VARCHAR(255) DEFAULT NULL');
         $this->addSql('ALTER TABLE security_group ADD version VARCHAR(255) DEFAULT NULL');
         $this->addSql('ALTER TABLE "user" ADD reference VARCHAR(255) DEFAULT NULL');
@@ -30,9 +46,17 @@ final class Version20230328151236 extends AbstractMigration
         $this->addSql('ALTER TABLE application ADD version VARCHAR(255) DEFAULT NULL');
     }
 
+
+    /**
+     * Down
+     *
+     * @param Schema $schema Schema
+     *
+     * @return void
+     */
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
+        // This down() migration is auto-generated, please modify it to your needs.
         $this->addSql('CREATE SCHEMA public');
         $this->addSql('ALTER TABLE security_group DROP reference');
         $this->addSql('ALTER TABLE security_group DROP version');
