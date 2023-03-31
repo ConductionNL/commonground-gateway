beaa930d85790a2f5766d83e540ade924b812dfc
Wilco Louwerse <wilco@conduction.nl>
2023-03-31 13:48:41 +0200

    Fix setting multiple type=object to null, [] or actual objects

--- api/src/Subscriber/ValueSubscriber.php
+++ api/src/Subscriber/ValueSubscriber.php
@@ -180,8 +180,6 @@ class ValueSubscriber implements EventSubscriberInterface
                 }
                 $valueObject->setArrayValue([]);
             } elseif ((Uuid::isValid($valueObject->getStringValue()) || filter_var($valueObject->getStringValue(), FILTER_VALIDATE_URL)) && $identifier = $valueObject->getStringValue()) {
-
-//                var_dump($identifier, $valueObject->getAttribute()->getName());
                 foreach ($valueObject->getObjects() as $object) {
                     $valueObject->removeObject($object);
                 }
