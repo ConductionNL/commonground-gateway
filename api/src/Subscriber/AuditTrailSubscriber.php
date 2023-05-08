155b3f2c0e6aab999ae2d887d25d6bfa3425f4b3
Robert Zondervan <robert@conduction.nl>
2023-05-08 13:19:50 +0200

    Few styling things

--- api/src/Subscriber/AuditTrailSubscriber.php
+++ api/src/Subscriber/AuditTrailSubscriber.php
@@ -4,22 +4,15 @@ namespace App\Subscriber;
 
 use App\Entity\AuditTrail;
 use App\Entity\ObjectEntity;
-use App\Entity\Synchronization;
-use App\Entity\Value;
-use App\Service\SynchronizationService;
 use CommonGateway\CoreBundle\Service\CacheService;
 use Doctrine\Bundle\DoctrineBundle\EventSubscriber\EventSubscriberInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Events;
-use Doctrine\ORM\NonUniqueResultException;
 use Doctrine\Persistence\Event\LifecycleEventArgs;
 use Psr\Log\LoggerInterface;
-use Ramsey\Uuid\Uuid;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
-use Symfony\Component\HttpKernel\Event\KernelEvent;
 use Symfony\Component\Security\Core\Security;
-use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 
 class AuditTrailSubscriber implements EventSubscriberInterface
 {
@@ -44,14 +37,14 @@ class AuditTrailSubscriber implements EventSubscriberInterface
     private ParameterBagInterface $parameterBag;
 
     /**
-     * The request stack
+     * The request stack.
      *
      * @var RequestStack
      */
     private RequestStack $requestStack;
 
     /**
-     * The cache service
+     * The cache service.
      *
      * @var CacheService
      */
@@ -90,7 +83,7 @@ class AuditTrailSubscriber implements EventSubscriberInterface
             Events::postUpdate,
             Events::postPersist,
             Events::preRemove,
-            Events::postLoad
+            Events::postLoad,
         ];
     }//end getSubscribedEvents()
 
@@ -104,7 +97,7 @@ class AuditTrailSubscriber implements EventSubscriberInterface
     {
         $userId = null;
 
-        if($this->security->getUser() !== null) {
+        if ($this->security->getUser() !== null) {
             $userId = $this->security->getUser()->getUserIdentifier();
         }
         $user = $this->entityManager->getRepository('App:User')->find($userId);
@@ -117,7 +110,7 @@ class AuditTrailSubscriber implements EventSubscriberInterface
         $auditTrail->setUserId('Anonymous');
         $auditTrail->setUserView('Anonymous');
 
-        if($user !== null) {
+        if ($user !== null) {
             $auditTrail->setApplicationId($user->getApplications()->first()->getId()->toString());
             $auditTrail->setApplicationView($user->getApplications()->first()->getName());
             $auditTrail->setUserId($userId);
@@ -149,7 +142,7 @@ class AuditTrailSubscriber implements EventSubscriberInterface
 
         $config = [
             'action' => 'READ',
-            'result' => 200
+            'result' => 200,
         ];
 
         $auditTrail = $this->createAuditTrail($object, $config);
@@ -171,13 +164,13 @@ class AuditTrailSubscriber implements EventSubscriberInterface
 
         $config = [
             'action' => 'UPDATE',
-            'result' => 200
+            'result' => 200,
         ];
 
         if($this->requestStack->getMainRequest()->getMethod() === 'PATCH') {
             $config = [
                 'action' => 'PARTIAL_UPDATE',
-                'result' => 200
+                'result' => 200,
             ];
         }
 
@@ -205,7 +198,7 @@ class AuditTrailSubscriber implements EventSubscriberInterface
 
         $config = [
             'action' => 'CREATE',
-            'result' => 201
+            'result' => 201,
         ];
 
         $auditTrail = $this->createAuditTrail($object, $config);
@@ -229,7 +222,7 @@ class AuditTrailSubscriber implements EventSubscriberInterface
 
         $config = [
             'action' => 'DELETE',
-            'result' => 204
+            'result' => 204,
         ];
         $auditTrail = $this->createAuditTrail($object, $config);
         $auditTrail->setAmendments([
