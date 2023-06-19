c84d5d091490a68f036048bfa04872f0b5d079a3
Robert Zondervan <robert@conduction.nl>
2023-06-19 14:23:07 +0200

    Reinstate larping bundle in bundles.php

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -15,6 +15,7 @@ return [
     Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class                       => ['all' => true],
     Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['all' => true],
     Hautelook\AliceBundle\HautelookAliceBundle::class                          => ['all' => true],
+    LarpingBase\LarpingBundle\LarpingBundle::class                             => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class                  => ['all' => true],
     Setono\CronExpressionBundle\SetonoCronExpressionBundle::class              => ['all' => true],
     CommonGateway\CoreBundle\CoreBundle::class                                 => ['all' => true],
