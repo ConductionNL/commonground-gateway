bd5b8c040d242ee5649625c43d315fb9812c80f9
Robert Zondervan <robert@conduction.nl>
2023-10-24 09:26:33 +0200

    Add creating an actionEvent on log

--- api/src/Logger/SessionDataProcessor.php
+++ api/src/Logger/SessionDataProcessor.php
@@ -2,6 +2,9 @@
 
 namespace App\Logger;
 
+use App\Event\ActionEvent;
+use Doctrine\ORM\EntityManagerInterface;
+use Psr\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
@@ -10,14 +13,34 @@ class SessionDataProcessor
     private SessionInterface $session;
     private RequestStack $requestStack;
 
-    public function __construct(SessionInterface $session, RequestStack $requestStack)
+    /**
+     * @var EventDispatcherInterface The event dispatcher.
+     */
+    private EventDispatcherInterface $eventDispatcher;
+
+    private EntityManagerInterface $entityManager;
+
+    /**
+     * @param SessionInterface $session
+     * @param RequestStack $requestStack
+     * @param EventDispatcherInterface $eventDispatcher
+     */
+    public function __construct(
+        SessionInterface $session,
+        RequestStack $requestStack,
+        EventDispatcherInterface $eventDispatcher,
+        EntityManagerInterface $entityManager
+    )
     {
         $this->session = $session;
         $this->requestStack = $requestStack;
+        $this->eventDispatcher = $eventDispatcher;
+        $this->entityManager = $entityManager;
     }
 
     public function __invoke(array $record): array
     {
+
         $record['context']['session'] = $this->session->getId();
         $record['context']['process'] = $this->session->has('process') ? $this->session->get('process') : '';
         $record['context']['endpoint'] = $this->session->has('endpoint') ? $this->session->get('endpoint') : '';
@@ -33,6 +56,20 @@ class SessionDataProcessor
         $record['context']['application'] = $this->session->has('application') ? $this->session->get('application') : '';
         $record['context']['host'] = $this->requestStack->getMainRequest() ? $this->requestStack->getMainRequest()->getHost() : '';
         $record['context']['ip'] = $this->requestStack->getMainRequest() ? $this->requestStack->getMainRequest()->getClientIp() : '';
+
+
+        if ($this->entityManager->getConnection()->isConnected() === true
+            && in_array(
+                $this->entityManager->getConnection()->getDatabase(),
+                $this->entityManager->getConnection()->getSchemaManager()->listDatabases()
+            ) === true
+        ){
+            $event = new ActionEvent('commongateway.action.event', $record, 'core.log.create');
+
+            $this->eventDispatcher->dispatch($event, 'commongateway.action.event');
+
+            $record = $event->getData();
+        }
 
         return $record;
     }
