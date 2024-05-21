d04edff3dff4850a7b5536b6a015c1d2def37c38
Robert Zondervan <robert@conduction.nl>
2024-05-21 10:03:56 +0200

    Add changes for elastic

---
+++ api/migrations/Version20240516090339.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240516090339 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE database ADD type VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE database ADD auth VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE database DROP type');
+        $this->addSql('ALTER TABLE database DROP auth');
+    }
+}
